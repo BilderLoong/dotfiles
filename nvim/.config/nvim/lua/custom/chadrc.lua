b4111e0f123c4d9bfa98d892aa00dde9f775270f
bilder <cokoryu@outlook.com>
2023-07-26 14:47:21 +0000

    M nvim/.config/nvim/lua/custom/chadrc.lua

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {
-  mappings = require('custom.mappings'),
-  plugins = "custom.plugins"
+  mappings = require "custom.mappings",
+  plugins = "custom.plugins",
 }
 
 -- check core.mappings for table structure
