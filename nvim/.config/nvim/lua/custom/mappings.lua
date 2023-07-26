0c1e7579f8f8a8c2ee6bfe2cde3f752661ffcd53
bilder <cokoryu@outlook.com>
2023-07-26 14:06:42 +0000

    M nvim/.config/nvim/lua/custom/mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -5,7 +5,6 @@ M.general = {
   i = {['jk']={"<ESC>", "escape insert mode", opts = {nowait=true}}}
 }
 
-local builtin = require('telescope.builtin')
 M.telescope = {
   n = {
     ["<leader>fs"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "Find Workspace symbols" },
