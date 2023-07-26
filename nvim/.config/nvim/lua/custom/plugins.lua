f11a687220bb5b8ffeab88c70fbcb3b9d7d3de60
bilder <cokoryu@outlook.com>
2023-07-26 13:22:20 +0000

    save

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -119,14 +119,20 @@ local plugins = {
         "html-lsp",
         "prettier",
         "typescript-language-server",
-        -- "cssls",
-        "vuejs/vetur ",
 
         -- MP
-        "wxml-languageserver",
         "yaml-language-server",
 
       },
+    },
+  }
+  ,{
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
     },
   }
 }
