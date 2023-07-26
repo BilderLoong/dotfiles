de695be7e537617ec40f4f1050ccdb619d6a2e37
bilder <cokoryu@outlook.com>
2023-07-26 14:44:23 +0000

    M nvim/.config/nvim/lua/custom/configs/null-ls.lua

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -3,6 +3,7 @@ local null_ls = require "null-ls"
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
+local lint = null_ls.builtins.
 
 local sources = {
   -- Web developement
