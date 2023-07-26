9dc2fbe0772ccc4e86185718be1a08771bbe6063
huzilong <huzilong@meituan.com>
2023-07-26 15:25:38 +0800

    M nvim/.config/nvim/lua/custom/configs/lspconfig.lua

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 local servers = { "html", "cssls", "clangd","tsserver"}
 for _, lsp in ipairs(servers) do
-  print('lsp')
+  print('lsp',lsp)
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
