241d27da161d5d04ec51e85f78be4ec65b58f117
bilder <cokoryu@outlook.com>
2023-07-26 13:52:34 +0000

    M nvim/.config/nvim/lua/custom/mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -8,7 +8,7 @@ M.general = {
 M.telescope = {
   n = {
 
-    ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+    ["<leader>ff"] = { "<cmd> Telescope workspace_symbol<CR>", "Find files" },
   }
 }
 
