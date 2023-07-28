5856518d2a4e71f1ea7f1e4b47888e5eb3ca0641
huzilong <huzilong@meituan.com>
2023-07-28 19:26:27 +0800

    save

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -5,16 +5,24 @@ M.general = {
 	i = { ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } } },
 }
 
+local telescope_i_n = {
+	["<A-d>"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Find document symbols" },
+	["<A-p>"] = { "<cmd> Telescope <CR>", "Telescope" },
+	["<A-m>"] = { "<cmd> Telescope keymaps <CR>", "Telescope keymaps" },
+	["<A-c>"] = { "<cmd> Telescope commands <CR>", "Telescope commands" },
+}
+
 M.telescope = {
-	n = {
+	n = vim.tbl_deep_extend("force", telescope_i_n, {
 		["<leader>fs"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Find workspace symbols" },
 		["<leader>fr"] = { "<cmd> Telescope resume <CR>", "Resume last Telescope picker" },
+	}),
+	i = vim.tbl_deep_extend("force", {}, {
 		["<A-d>"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Find document symbols" },
 		["<A-p>"] = { "<cmd> Telescope <CR>", "Telescope" },
 		["<A-m>"] = { "<cmd> Telescope keymaps <CR>", "Telescope keymaps" },
 		["<A-c>"] = { "<cmd> Telescope commands <CR>", "Telescope commands" },
-	},
-	i = {},
+	}),
 }
 
 return M
