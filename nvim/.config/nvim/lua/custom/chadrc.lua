08a8df5a52b2d50051c4065a903e875928ce6d2a
huzilong <huzilong@meituan.com>
2023-07-28 15:24:06 +0800

    ?? nvim/.config/nvim/lua/custom/chadrc.lua ?? nvim/.config/nvim/lua/custom/configs/lspconfig.lua ?? nvim/.config/nvim/lua/custom/configs/null-ls.lua ?? nvim/.config/nvim/lua/custom/mappings.lua ?? nvim/.config/nvim/lua/custom/plugins.lua ?? nvim/.config/nvim/lua/custom/vscode.lua

---
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,9 @@
+---@type ChadrcConfig
+local M = {
+  mappings = require "custom.mappings",
+  plugins = "custom.plugins",
+}
+
+-- check core.mappings for table structure
+
+return M
---
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -0,0 +1,25 @@
+local null_ls = require "null-ls"
+
+local fmt = null_ls.builtins.formatting
+local lint = null_ls.builtins.diagnostics
+local ca = null_ls.builtins.code_actions
+
+local sources = {
+  -- Web developement
+  fmt.prettierd,
+  ca.eslint_d,
+  ca.gitrebase,
+
+  fmt.stylua,
+
+  lint.shellcheck,
+
+  -- Misc
+  -- null_ls.builtins.diagnostics.cspell,
+  -- null_ls.builtins.code_actions.cspell,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
