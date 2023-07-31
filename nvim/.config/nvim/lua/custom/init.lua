8ea00c694b705bc0fe24ee3207727682957d24d3
huzilong <huzilong@meituan.com>
2023-07-31 14:57:24 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -5,9 +5,5 @@ local g = vim.g
 vim.opt.relativenumber = true
 
 -- Folding
-vim.opt.foldlevel = 20
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitgre#sbydexpr()"
-vim.opt.foldenable = true
 
 -- opt.autochdir = true
