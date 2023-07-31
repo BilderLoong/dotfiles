31f51b053c55133a7f5089a8702d25f4b10dce2e
huzilong <huzilong@meituan.com>
2023-07-31 16:26:40 +0800

    M nvim/.config/nvim/lua/custom/mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -26,8 +26,7 @@ local telescope_i_n = {
 	["<A-m>"] = { "<cmd> Telescope keymaps <CR>", "Telescope keymaps" },
 	["<A-c>"] = { "<cmd> Telescope commands <CR>", "Telescope commands" },
 	["<A-r>"] = {
-		"<cmd> Telescope lsp_references <CR>",
-		"Telescope LSP references",
+ "<cmd> Telescope resume <CR>", "Resume last Telescope picker" 
 	},
 }
 
