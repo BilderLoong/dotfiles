44a15195d94a593d183713f00daf94ba5bf8595c
huzilong <huzilong@meituan.com>
2023-07-31 14:08:55 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -5,16 +5,7 @@ local plugins = {
 	{
 		"unblevable/quick-scope",
 		event = BufEnterLike,
-		config = function()
-			vim.highlight.create(
-				"QuickScopePrimary",
-				{ guifg = "#afff5f", gui = "underline", ctermfg = 155, cterm = "underline" }
-			)
-			vim.highlight.create(
-				"QuickScopeSecondary",
-				{ guifg = "#6fffff", gui = "underline", ctermfg = 81, cterm = "underline" }
-			)
-		end,
+		config =,
 	},
 	{
 		"ggandor/lightspeed.nvim",
