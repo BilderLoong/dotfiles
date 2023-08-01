eb47a602a81d6efeafb8a8bbb37582d9965309b4
huzilong <huzilong@meituan.com>
2023-08-01 14:57:51 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -221,9 +221,19 @@ local plugins = {
 
 	{
 		"rmagatti/auto-session",
-    event="VeryLazy",
+		-- event = "VeryLazy",
 		opts = {
-			log_level = "error",
+			log_level = vim.log.levels.ERROR,
+			auto_session_enable_last_session = true,
+
+			-- ⚠️ This will only work if Telescope.nvim is installed
+			-- The following are already the default values, no need to provide them if these are already the settings you want.
+			session_lens = {
+				-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+				load_on_setup = true,
+				theme_conf = { border = true },
+				previewer = true,
+			},
 		},
 		config = function(_, opts)
 			require("auto-session").setup(opts)
