376ed1fe5de3d730930dec147c83c5dcece56d71
huzilong <huzilong@meituan.com>
2023-08-01 11:32:06 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -209,6 +209,6 @@ local plugins = {
 	-- 	end,
 	-- },
    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-},
+}
 
 return plugins
