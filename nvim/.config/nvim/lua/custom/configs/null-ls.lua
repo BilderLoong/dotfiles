070a432c3eb9ad9256648c616f0f0e2e99fffd54
huzilong <huzilong@meituan.com>
2023-08-03 10:37:39 +0800

    M nvim/.config/nvim/lua/custom/configs/null-ls.lua

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -1,32 +1,32 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local fmt = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 local ca = null_ls.builtins.code_actions
 
 local sources = {
-  -- Web developement
-  fmt.prettierd,
-  -- fmt.prettier,
-  ca.eslint_d,
-  ca.gitrebase,
+	-- Web developement
+	fmt.prettierd,
+	-- fmt.prettier,
+	ca.eslint_d,
+	ca.gitrebase,
 
-  fmt.stylua,
+	fmt.stylua,
 
-  -- Shell
-  lint.shellcheck,
-  fmt.shfmt,
+	-- Shell
+	lint.shellcheck,
+	fmt.shfmt,
 
-  -- Python
-  fmt.black,
-  lint.ruff,
+	-- Python
+	fmt.black,
+	lint.ruff,
 
-  -- Misc
-  -- null_ls.builtins.diagnostics.cspell,
-  -- null_ls.builtins.code_actions.cspell,
+	-- Misc
+	-- null_ls.builtins.diagnostics.cspell,
+	-- null_ls.builtins.code_actions.cspell,
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	-- debug = true,
+	sources = sources,
+})
