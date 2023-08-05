20ba61a960d561c32f5c54fcc6d759c668f40548
bilder <cokoryu@outlook.com>
2023-08-05 12:02:25 +0800

    M zsh/zsh/custom/macOS.sh

--- zsh/zsh/custom/macOS.sh
+++ zsh/zsh/custom/macOS.sh
@@ -1,3 +1,15 @@
 
 # Android SDK
 export ANDROID_HOME=~/Library/Android/sdk
+
+# The wechat mp devtool cli path: https://segmentfault.com/a/1190000040867117 .
+wechatcli () {
+  wechatcli='/Applications/wechatwebdevtools.app/Contents/MacOS/cli'
+
+  if [[ -z "$1" ]] 
+  then
+    "${wechatcli}"
+  else
+   ${wechatcli} open --project ~/Project/menuorder-new-rms-h5-$1/apps/menuorder/dist/merchant
+  fi
+}
