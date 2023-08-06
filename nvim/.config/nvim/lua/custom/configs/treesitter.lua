991aab6416456fbdb2a70314d8bc7ac2aab9f518
bilder <cokoryu@outlook.com>
2023-08-06 14:06:31 +0000

    M nvim/.config/nvim/lua/custom/configs/treesitter.lua

--- nvim/.config/nvim/lua/custom/configs/treesitter.lua
+++ nvim/.config/nvim/lua/custom/configs/treesitter.lua
@@ -1,27 +1,27 @@
 local options = {
 	ensure_installed = "all",
 
-	-- highlight = {
-	-- 	--- Disable syntax highlight for big file.
-	-- 	--- @param lang string
-	-- 	--- @param bufnr number
-	-- 	--- @return boolean
-	-- 	disable = function(lang, bufnr)
-	-- 		-- :h uv.fs_stat()
-	-- 		local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-	--
-	-- 		if ok and stats then
-	-- 			return true
-	-- 		end
-	--
-	-- 		local buf_size = stats.size
-	-- 		vim.print("size", buf_size)
-	-- 		-- Disable big oneline file.
-	-- 		local is_big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and buf_size > 100 * 1024
-	-- 		vim.print("is_big_oneliner", is_big_oneliner)
-	-- 		return is_big_oneliner or buf_size > 100 * 1024 -- 100 KB
-	-- 	end,
-	-- },
+	highlight = {
+		--- Disable syntax highlight for big file.
+		--- @param lang string
+		--- @param bufnr number
+		--- @return boolean
+		disable = function(lang, bufnr)
+			-- :h uv.fs_stat()
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+
+			if ok and stats then
+				return true
+			end
+
+			local buf_size = stats.size
+			vim.print("size", buf_size)
+			-- Disable big oneline file.
+			local is_big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and buf_size > 100 * 1024
+			vim.print("is_big_oneliner", is_big_oneliner)
+			return is_big_oneliner or buf_size > 100 * 1024 -- 100 KB
+		end,
+	},
 	-- ensure_installed = {
 	-- 	-- defaults
 	-- 	"vim",
