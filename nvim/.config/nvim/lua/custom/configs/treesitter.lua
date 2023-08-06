b2e7a1421a8582471f49fce4d05685fa79f4691c
bilder <cokoryu@outlook.com>
2023-08-06 13:55:01 +0000

    M nvim/.config/nvim/lua/custom/configs/treesitter.lua

--- nvim/.config/nvim/lua/custom/configs/treesitter.lua
+++ nvim/.config/nvim/lua/custom/configs/treesitter.lua
@@ -3,9 +3,9 @@ local options = {
 
 	highlight = {
 		--- Disable syntax highlight for big file.
-    ---@param lang string
-    ---@param bufnr number
-    ---@return boolean
+		---@param lang string
+		---@param bufnr number
+		---@return boolean
 		disable = function(lang, bufnr)
 			-- :h uv.fs_stat()
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
@@ -15,9 +15,10 @@ local options = {
 			end
 
 			local buf_size = stats.size
-      print('size')
+			print("size", buf_size)
 			-- Disable big oneline file.
 			local is_big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and buf_size > 100 * 1024
+			print("size", buf_size)
 			return is_big_oneliner or buf_size > 100 * 1024 -- 100 KB
 		end,
 	},
