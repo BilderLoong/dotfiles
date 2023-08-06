33b3e77a44efe2200cdcd1cf723b6b9f81d3dbe3
bilder <cokoryu@outlook.com>
2023-08-06 13:43:00 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -262,8 +262,8 @@ local plugins = {
 		end,
 	},
 	{
-    event="VeryLazy",
 		"christoomey/vim-tmux-navigator",
+    event="VeryLazy",
 		init = function()
 			-- Write all buffers before navigating from Vim to tmux pane
 			vim.g.tmux_navigator_save_on_switch = 2
