bebfa0ade51ee3bfcd4a7f7bbb95c14cc4c8569f
bilder <cokoryu@outlook.com>
2023-08-06 13:34:04 +0000

    M nvim/.config/nvim/lua/custom/configs/treesitter.lua

--- nvim/.config/nvim/lua/custom/configs/treesitter.lua
+++ nvim/.config/nvim/lua/custom/configs/treesitter.lua
@@ -3,7 +3,7 @@ local options = {
 	highlight = {
 		disable = function(lang, bufnr)
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-			local big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and stats.size > 1000 * 1024
+			local big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and stats.size > 100 * 1024
 			-- :h uv.fs_stat()
 		end,
 	},
