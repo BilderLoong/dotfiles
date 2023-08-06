1077025d57ce215af1b8baf3d7b499d4e205dba3
bilder <cokoryu@outlook.com>
2023-08-06 13:34:55 +0000

    M nvim/.config/nvim/lua/custom/configs/treesitter.lua

--- nvim/.config/nvim/lua/custom/configs/treesitter.lua
+++ nvim/.config/nvim/lua/custom/configs/treesitter.lua
@@ -3,6 +3,7 @@ local options = {
 	highlight = {
 		disable = function(lang, bufnr)
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+local size
       -- Disable big oneline file.
 			-- :h uv.fs_stat()
 			local is_big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and stats.size > 100 * 1024
