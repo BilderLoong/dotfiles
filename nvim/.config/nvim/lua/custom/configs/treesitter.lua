f2081a0118cf42a4198144d7581efa2e0d5caac8
bilder <cokoryu@outlook.com>
2023-08-06 13:31:32 +0000

    M nvim/.config/nvim/lua/custom/configs/treesitter.lua

--- nvim/.config/nvim/lua/custom/configs/treesitter.lua
+++ nvim/.config/nvim/lua/custom/configs/treesitter.lua
@@ -4,6 +4,7 @@ local options = {
 		disable = function(lang, bufnr)
 			local big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and 
 
+--
 local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 		end,
 	},
