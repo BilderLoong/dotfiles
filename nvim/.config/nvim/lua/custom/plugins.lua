184020ac92c6cb436c17170aafed7d36e1d31d0d
bilder <cokoryu@outlook.com>
2023-08-06 13:45:26 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -68,24 +68,6 @@ local plugins = {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
-			ensure_installed = {
-				-- defaults
-				"vim",
-				"lua",
-				-- web dev
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"tsx",
-				"json",
-				"go",
-				-- "vue", "svelte"
-
-				-- low level
-				"c",
-				"zig",
-			},
 		},
 	},
 	{
