8d6e52137bea9c6db5de33a1c03c55180188e04f
bilder <cokoryu@outlook.com>
2023-08-06 13:47:58 +0000

    M nvim/.config/nvim/lua/custom/configs/treesitter.lua

--- nvim/.config/nvim/lua/custom/configs/treesitter.lua
+++ nvim/.config/nvim/lua/custom/configs/treesitter.lua
@@ -4,7 +4,9 @@ local options = {
 	highlight = {
 		--- Disable syntax highlight for big file.
 		--- bufnr: buffer number.
-    
+    ---@param lang any
+    ---@param bufnr any
+    ---@return boolean
 		disable = function(lang, bufnr)
 			-- :h uv.fs_stat()
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
