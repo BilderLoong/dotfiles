527d1b344ad905f39a8a4c05be313c0cc40f34ae
huzilong <huzilong@meituan.com>
2023-08-08 10:51:11 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -264,7 +264,7 @@ local plugins = {
 	{
 		"numToStr/Comment.nvim",
 		opts = {
-			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			-- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 		},
 	},
 }
