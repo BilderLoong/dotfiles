7d9e48d9b4594aa7f1bd3dbad97e47198c95167a
huzilong <huzilong@meituan.com>
2023-08-08 14:51:23 +0800

    M nvim/.config/nvim/lua/custom/configs/treesitter.lua

--- nvim/.config/nvim/lua/custom/configs/treesitter.lua
+++ nvim/.config/nvim/lua/custom/configs/treesitter.lua
@@ -1,5 +1,4 @@
 local options = {
-  ensure_installed = "all",
 
   highlight = {
     --- Disable syntax highlight for big file.
@@ -86,24 +85,35 @@ local options = {
     enable = true,
     enable_autocmd = false,
   },
-  -- ensure_installed = {
-  -- 	-- defaults
-  -- 	"vim",
-  -- 	"lua",
-  -- 	-- web dev
-  -- 	"html",
-  -- 	"css",
-  -- 	"javascript",
-  -- 	"typescript",
-  -- 	"tsx",
-  -- 	"json",
-  -- 	"go",
-  -- 	-- "vue", "svelte"
-  --
-  -- 	-- low level
-  -- 	"c",
-  -- 	"zig",
-  -- },
+
+    -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+  ensure_installed = {
+  	-- defaults
+  	"vim",
+  	"lua",
+  	-- web dev
+  	"html",
+  	"css",
+  	"javascript",
+  	"typescript",
+  	"tsx",
+  	"json",
+  	"go",
+  	-- "vue", "svelte"
+
+  	-- low level
+  	"c",
+  	"zig",
+  },
+    -- ensure_installed = "all",
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
 }
 
 return options
