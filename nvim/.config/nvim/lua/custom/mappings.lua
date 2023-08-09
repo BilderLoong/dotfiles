951fd9eae4ceff18e531c1211ecec80c4bbe5283
huzilong <huzilong@meituan.com>
2023-08-09 11:30:55 +0800

    M nvim/.config/nvim/lua/custom/mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -24,7 +24,7 @@ local telescope_i_n = {
 	["<A-d>"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Find document symbols" },
 	["<A-p>"] = { "<cmd> Telescope <CR>", "Telescope" },
 	["<A-m>"] = { "<cmd> Telescope keymaps <CR>", "Telescope keymaps" },
-	["<A-c>"] = { "<cmd> Telescope commands <CR>", "Telescope commands" },
+	["<A-p>"] = { "<cmd> Telescope commands <CR>", "Telescope commands" },
 	["<A-r>"] = { "<cmd> Telescope resume <CR>", "Resume last Telescope picker" },
 }
 
