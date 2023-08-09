035644a6a1e10d25c75b217707ac1fc8433358df
huzilong <huzilong@meituan.com>
2023-08-09 14:18:34 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -12,7 +12,7 @@ opt.relativenumber = true
 g.toggle_theme_icon = ""
 
 vim.cmd([[
-set foldmethod=expr
-set foldexpr=nvim_treesitter#foldexpr()
-set nofoldenable                     " Disable folding at startup.
+  set foldmethod=expr
+  set foldexpr=nvim_treesitter#foldexpr()
+  " set nofoldenable                     " Disable folding at startup.
 ]])
