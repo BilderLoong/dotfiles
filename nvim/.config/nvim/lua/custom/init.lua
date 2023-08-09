006e6cc145edf9bd4e55260ce985591572b78469
huzilong <huzilong@meituan.com>
2023-08-09 14:22:15 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -14,8 +14,3 @@ g.toggle_theme_icon = ""
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 
-vim.cmd([[
-  set foldmethod=expr
-  set foldexpr=nvim_treesitter#foldexpr()
-  " set nofoldenable                     " Disable folding at startup.
-]])
