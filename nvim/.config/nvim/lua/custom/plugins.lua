e9a8a571b509729cbf4e6a04735b07ff5891c209
huzilong <huzilong@meituan.com>
2023-08-11 13:29:41 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -65,7 +65,7 @@ local plugins = {
 	{
 		"simrat39/symbols-outline.nvim",
 		event = BufEnterLike,
-    keys = ""<
+    keys = "",
 		-- config = function()
 		-- 	require("symbols-outline").setup()
 		-- end,
