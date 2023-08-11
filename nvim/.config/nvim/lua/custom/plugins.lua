486d5504cda4740e84655aff503d9e6a4d7875a3
huzilong <huzilong@meituan.com>
2023-08-11 10:00:55 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -203,9 +203,9 @@ local plugins = {
 	},
 	{
 		"hrsh7th/nvim-cmp",
-		config = function(_, opt)
-			require("cmp").setup(opt)
-		end,
+		-- config = function(_, opt)
+		-- 	require("cmp").setup(opt)
+		-- end,
 		opts = function()
 			local cmp = require("cmp")
 			local opts = vim.tbl_deep_extend("force", require("plugins.configs.cmp"), {
