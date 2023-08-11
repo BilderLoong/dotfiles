8834d2c68c1ed72a5a66ca6215b18147f60b449e
huzilong <huzilong@meituan.com>
2023-08-11 13:30:14 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -65,7 +65,7 @@ local plugins = {
 	{
 		"simrat39/symbols-outline.nvim",
 		event = BufEnterLike,
-		cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+		cmd = { "SymbolsOutline", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
 		-- config = function()
 		-- 	require("symbols-outline").setup()
 		-- end,
