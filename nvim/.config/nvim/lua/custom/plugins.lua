ec71b10c6e371372e0a488bb63230250c43a868a
huzilong <huzilong@meituan.com>
2023-08-11 13:40:12 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -199,11 +199,31 @@ local plugins = {
 			local defaults = require("plugins.configs.others").gitsigns
 			local custom = {
 				signs = {
-add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+					add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+					change = {
+						hl = "GitSignsChange",
+						text = "~",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+					delete = {
+						hl = "GitSignsDelete",
+						text = "_",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					topdelete = {
+						hl = "GitSignsDelete",
+						text = "‾",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					changedelete = {
+						hl = "GitSignsChange",
+						text = "│",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
 				},
 				current_line_blame = true,
 			}
