d1f674b24025e110c13d4b8308612acb40464be2
huzilong <huzilong@meituan.com>
2023-08-11 13:36:14 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -262,7 +262,7 @@ local plugins = {
 		opts = function()
 			return vim.tbl_deep_extend("force", require("plugins.configs.nvimtree"), {
 				filesystem_watchers = {
-					enable = false, --- I get a filesystem_watchers related error when compiling project.
+					enable = false, --- I get a filesystem_watchers related error when compiling project, disable it help to resolve the error.
 				},
 			})
 		end,
