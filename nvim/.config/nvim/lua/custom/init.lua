c1c3081a2f6c8ae6377dacc136155d8088a9a5bf
huzilong <huzilong@meituan.com>
2023-08-14 10:49:08 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -21,6 +21,7 @@ local function _start_tsserver()
 	-- local cwd = vim.loop.cmd()
 	local root_dir =
 		vim.fs.dirname(vim.fs.find({ "tsconfig.json", "package.json", "jsconfig.json", ".git" }, { upward = true })[1])
+
 	local client = vim.lsp.start({
 		name = "tsserver",
 		cmd = { "typescript-language-server", "--stdio" },
