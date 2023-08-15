083ef7316b1450fc09be87449708282960cc54a8
huzilong <huzilong@meituan.com>
2023-08-15 16:08:40 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -18,28 +18,28 @@ vim.cmd([[
 ]])
 
 
--- local function _start_tsserver()
--- 	--local cwd = vim.loop.cmd()
--- 	local root_dir =
--- 		vim.fs.dirname(vim.fs.find({ "tsconfig.json", "package.json", "jsconfig.json", ".git" }, { upward = true })[1])
---
--- 	vim.print("root_dir", root_dir)
---
--- 	local client = vim.lsp.start({
--- 		name = "tsserver",
--- 		cmd = { "typescript-language-server", "--stdio" },
--- 		root_dir = root_dir,
--- 	})
--- end
---
--- if vim.g.vim_did_enter then
--- 	_start_tsserver()
--- else
--- 	vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
--- 		--pattern = "*",
--- 		callback = function()
--- 			_start_tsserver()
--- 		end,
--- 	})
--- end
---
+local function _start_tsserver()
+	--local cwd = vim.loop.cmd()
+	local root_dir =
+		vim.fs.dirname(vim.fs.find({ "tsconfig.json", "package.json", "jsconfig.json", ".git" }, { upward = true })[1])
+
+	vim.print("root_dir", root_dir)
+
+	local client = vim.lsp.start({
+		name = "tsserver",
+		cmd = { "typescript-language-server", "--stdio" },
+		root_dir = root_dir,
+	})
+end
+
+if vim.g.vim_did_enter then
+	_start_tsserver()
+else
+	vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+		--pattern = "*",
+		callback = function()
+			_start_tsserver()
+		end,
+	})
+end
+
