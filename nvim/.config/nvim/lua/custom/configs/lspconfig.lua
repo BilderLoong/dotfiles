e53b5af4c0087b0dcced7769b1ba77e5273794ce
huzilong <huzilong@meituan.com>
2023-08-15 15:36:02 +0800

    M nvim/.config/nvim/lua/custom/configs/lspconfig.lua

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 local configs = require("plugins.configs.lspconfig")
 local lspconfig = require("lspconfig")
 
-vim.print(require(lspconfig.lspconfig.util.lua))
+vim.print(require(lspconfig.lspconfig.util))
 
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
