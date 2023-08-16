ea33942041aa41d05cf8d0ad6197f79d0510ea8d
huzilong <huzilong@meituan.com>
2023-08-16 13:50:26 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -288,6 +288,21 @@ local plugins = {
 			vim.g.tmux_navigator_save_on_switch = 2
 		end,
 	},
+
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
 }
 
 return plugins
