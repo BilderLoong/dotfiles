fc0d817bebcb7d599c815c4e94b03101173c2865
huzilong <huzilong@meituan.com>
2023-08-18 14:09:14 +0800

    M nvim/.config/nvim/lua/custom/autocmds.lua

--- nvim/.config/nvim/lua/custom/autocmds.lua
+++ nvim/.config/nvim/lua/custom/autocmds.lua
@@ -3,7 +3,7 @@ local api = vim.api
 api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
 	pattern = "*",
 	command = "silent! wall",
-  callback = function (ctx)
-   vim.print('Save all buffer on blur.') 
-  end
+	callback = function(ctx)
+		vim.print("Save all buffer on blur.")
+	end,
 })
