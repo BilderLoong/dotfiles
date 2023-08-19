a0cadcb08e2cd1d7ffece46c092116a3f23b401c
bilder <cokoryu@outlook.com>
2023-08-19 02:21:24 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -359,6 +359,10 @@ local plugins = {
      config = function()
         require('lspsaga').setup({})
     end,
+     dependencies = {
+        'nvim-treesitter/nvim-treesitter' -- optional
+        'nvim-tree/nvim-web-devicons'     -- optional
+    }
   }
 }
 
