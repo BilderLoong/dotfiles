4f90e4cd25bda923fcc3d6a2ea5bf2291fed97d5
huzilong <huzilong@meituan.com>
2023-08-23 17:14:36 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -62,7 +62,8 @@ zinit wait lucid for \
     #   multisrc"shell/{completion,key-bindings}.zsh" \
     #   id-as"junegunn/fzf_completions" \
     # pick"/dev/null" junegunn/fzf 
-zinit
+zinit load for \
+
 
 # Fish-like autosuggestions for zsh 
 zinit wait lucid atload'_zsh_autosuggest_start' for \
