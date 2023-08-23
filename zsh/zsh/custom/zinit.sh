1e36786ede6f12f375a5c18fb5437ea907995ea4
huzilong <huzilong@meituan.com>
2023-08-23 17:34:21 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -61,7 +61,7 @@ zinit wait lucid for \
     #   id-as"junegunn/fzf_completions" \
     # pick"/dev/null" junegunn/fzf 
 
-zinit for wait lucid has'ast-grep' id-as'sg-completions' atload'zicompinit; zicdreplay' \
+zinit for wait lucid ha
    zdharma-continuum/null
 
 # Fish-like autosuggestions for zsh 
