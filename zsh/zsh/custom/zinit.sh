67218adde77b4ce8529aa607b4829089b3bab667
huzilong <huzilong@meituan.com>
2023-08-23 18:23:40 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -55,7 +55,7 @@ zinit wait lucid for \
          has'navi' id-as'navi-widget' eval"navi widget zsh" \
     zdharma-continuum/null \
         has'ast-grep' id-as'sg-completions' \
-    zdharma-continuum/null\
+    zdharma-continuum/null
 
     #   atclone"shell/key-bindings.zsh" \
     #   atpull"%atclone" \
