04f873dccbd9de5219f05894597fe3e1655e1775
huzilong <huzilong@meituan.com>
2023-08-23 17:17:36 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -61,7 +61,7 @@ zinit wait lucid for \
     #   id-as"junegunn/fzf_completions" \
     # pick"/dev/null" junegunn/fzf 
 
-zinit wait lucid for \
+zinit ice 
          has'ast-grep' id-as'sg-completions' eval"ast-grep completions zsh" \
     zdharma-continuum/null 
 
