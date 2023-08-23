8d287bd18fab3f2b943c8d400f18ce98a5a0a349
huzilong <huzilong@meituan.com>
2023-08-23 18:22:26 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -52,7 +52,7 @@ zinit wait lucid for \
     zdharma-continuum/null  \
       has"thefuck" id-as"thefuck_init" eval"thefuck --alias" \
     zdharma-continuum/null \
-         has'navi' id-as'navi-widget' eval"navi widget zsh" \1000
+         has'navi' id-as'navi-widget' eval"navi widget zsh" \
     zdharma-continuum/null \
         has'ast-grep' id-as'sg-completions' \
     zdharma-continuum/null
