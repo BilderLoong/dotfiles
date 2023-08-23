5e24a50f30171bea757e5bd11df633fead08c729
huzilong <huzilong@meituan.com>
2023-08-23 18:25:13 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -52,13 +52,13 @@ zinit wait lucid for \
     zdharma-continuum/null  \
       has"thefuck" id-as"thefuck_init" eval"thefuck --alias" \
     zdharma-continuum/null \
-Aloxaf/fzf-tab
+    Aloxaf/fzf-tab\
          has'navi' id-as'navi-widget' eval"navi widget zsh" \
     zdharma-continuum/null \
         has'ast-grep' id-as'sg-completions' \
     zdharma-continuum/null
 
-zinit light 
+
 
     #   atclone"shell/key-bindings.zsh" \
     #   atpull"%atclone" \
