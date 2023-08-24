04179c3e2523536a46df5a2a1aced3ed466679c5
huzilong <huzilong@meituan.com>
2023-08-24 17:27:46 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -299,7 +299,7 @@ local plugins = {
 			-- https://github.com/rmagatti/auto-session#-command-hooks
 			-- https://www.reddit.com/r/neovim/comments/15bfz5f/how_to_open_nvim_tree_after_restoring_a_session/
 			pre_save_cmds = {
-				'
+	[[
 lua << EOF
   local tbl = {1, 2, 3}
   for k, v in ipairs(tbl) do
