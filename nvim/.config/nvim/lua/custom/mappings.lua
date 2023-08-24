f2c20058168b1c9295325c1bcc8287cb7845fd05
huzilong <huzilong@meituan.com>
2023-08-24 17:40:54 +0800

    M nvim/.config/nvim/lua/custom/mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -46,7 +46,7 @@ local telescope_i_n = {
 M.telescope = {
 	n = vim.tbl_deep_extend("force", telescope_i_n, {
 		["<leader>fs"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Find workspace symbols" },
-		["<leader>ss"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Find session histor" },
+		["<leader>ss"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Find session history" },
 		["<leader>sg"] = { "<cmd> Telescope ast_grep  <CR>", "AST Grep!" },
 		["<leader>ff"] = {
 			function()
