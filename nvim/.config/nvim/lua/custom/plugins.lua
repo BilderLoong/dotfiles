847dcd8cc32a325b1ccd70b12bb154cffa3f9558
huzilong <huzilong@meituan.com>
2023-08-24 16:44:06 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -414,7 +414,8 @@ local plugins = {
 		"rmagatti/session-lens",
 		dependencies = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
 		config = function()
-			require("session-lens").setup({--[[your custom config--]]
+			require("session-lens").setup({
+        
 			})
 		end,
 	},
