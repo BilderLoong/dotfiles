1a4acc5a7903367d3e1f82e5b728078aeb30f598
huzilong <huzilong@meituan.com>
2023-08-24 15:43:02 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -386,6 +386,9 @@ local plugins = {
       "rcarriga/nvim-dap-ui",
       "theHamsta/nvim-dap-virtual-text",
     },
+    config = function (_, opts)
+      
+    end
   },
   {
     "jbyuki/one-small-step-for-vimkind",
