3bd47fc0a242a89e139c40259ebed1af8f1d4c2a
huzilong <huzilong@meituan.com>
2023-08-24 11:08:31 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -374,6 +374,7 @@ local plugins = {
 			require("lspsaga").setup({})
 		end,
 		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
 			"nvim-tree/nvim-web-devicons", -- optional
 		},
 	},
