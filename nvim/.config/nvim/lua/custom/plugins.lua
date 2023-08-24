221faf6db2088d4c5f6e5b79ecd6ebee302ca0c8
huzilong <huzilong@meituan.com>
2023-08-24 17:33:29 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -299,7 +299,7 @@ local plugins = {
 			-- https://github.com/rmagatti/auto-session#-command-hooks
 			-- https://www.reddit.com/r/neovim/comments/15bfz5f/how_to_open_nvim_tree_after_restoring_a_session/
 			pre_save_cmds = {
-				[[ lua if vim.fn.exists(':NvimTreeClose') then vim.cmd('NvimTreeClose') ]],
+				[[lua if vim.fn.exists(':NvimTreeClose') then vim.cmd('NvimTreeClose') ]],
 			},
 		},
 		config = function(_, opts)
