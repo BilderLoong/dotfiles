de5ab7a6beee5634e85ffb9326d5b6e8beffb0d6
huzilong <huzilong@meituan.com>
2023-08-24 10:17:18 +0800

    M nvim/.config/nvim/lua/custom/mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -37,7 +37,7 @@ local telescope_i_n = {
 	["<A-r>"] = { "<cmd> Telescope resume <CR>", "Resume last Telescope picker" },
 	["<F1>"] = {
 		function()
-			require("fzf-lua").
+      require("fzf-lua").help_tag()
 		end,
 	},
 }
