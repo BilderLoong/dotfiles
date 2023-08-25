1abdbb02a265a8b45956ccc0f56596e71aa7b457
huzilong <huzilong@meituan.com>
2023-08-25 14:06:41 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -388,6 +388,7 @@ local plugins = {
 	{
 		-- https://github.com/mfussenegger/nvim-dap
 		"mfussenegger/nvim-dap",
+    keys = require()
 		dependencies = {
 			-- UI related https://github.com/mfussenegger/nvim-dap#goals
 			"rcarriga/nvim-dap-ui",
