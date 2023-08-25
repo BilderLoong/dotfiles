bedba408be5fc980b2d05be6c67412b2a7d50943
huzilong <huzilong@meituan.com>
2023-08-25 14:09:21 +0800

    M nvim/.config/nvim/lua/custom/utils.lua

--- nvim/.config/nvim/lua/custom/utils.lua
+++ nvim/.config/nvim/lua/custom/utils.lua
@@ -1,6 +1,12 @@
 local M = { }
 
-function M.
+function M.function getTableKeys(tab)
+  local keyset = {}
+  for k,v in pairs(tab) do
+    keyset[#keyset + 1] = k
+  end
+  return keyset
+end
 ()
   
 end
