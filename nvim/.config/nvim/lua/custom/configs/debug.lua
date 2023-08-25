41c54e71d095a716b4cfd2b9b7c4eacca972a351
huzilong <huzilong@meituan.com>
2023-08-25 14:58:59 +0800

    M nvim/.config/nvim/lua/custom/configs/debug.lua

--- nvim/.config/nvim/lua/custom/configs/debug.lua
+++ nvim/.config/nvim/lua/custom/configs/debug.lua
@@ -1,27 +1,23 @@
 local M = {}
 
 function M.setup()
-	local dap = require("dap")
+  local dap = require "dap"
 
-	-- https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt#L171
-	dap.adapters.nlua = function(callback, config)
-		callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
-	end
+  -- https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt#L171
+  dap.adapters.nlua = function(callback, config)
+    callback { type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 }
+  end
 
-	dap.configurations.lua = {
-		{
-			type = "nlua",
-			request = "attach",
-			name = "Attach to running Neovim instance",
-		},
-	}
+  dap.configurations.lua = {
+    {
+      type = "nlua",
+      request = "attach",
+      name = "Attach to running Neovim instance",
+    },
+  }
 
-	vim.api.nvim_set_keymap("n", "<F9>", [[:lua require"dap".toggle_breakpoint()<CR>]], { noremap = true })
-	vim.api.nvim_set_keymap("n", "<F9>", [[:lua require"dap".continue()<CR>]], { noremap = true })
-	vim.api.nvim_set_keymap("n", "<F10>", [[:lua require"dap".step_over()<CR>]], { noremap = true })
-	vim.api.nvim_set_keymap("n", "<F11>", [[:lua require"dap".step_into()<CR>]], { noremap = true })
-	vim.api.nvim_set_keymap("n", "<F12>", [[:lua require"dap.ui.widgets".hover()<CR>]], { noremap = true })
-	vim.api.nvim_set_keymap("n", "<F5>", [[:lua require"osv".launch({port = 8086})<CR>]], { noremap = true })
+  vim.api.nvim_set_keymap("n", "<F12>", [[:lua require"dap.ui.widgets".hover()<CR>]], { noremap = true })
+  vim.api.nvim_set_keymap("n", "<F2>", [[:lua require"osv".launch({port = 8086})<CR>]], { noremap = true })
 end
 
 return M
