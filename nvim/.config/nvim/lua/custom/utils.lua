373d1776907b5c1a5b5448dd25d4b3fff98fde8f
bilder <cokoryu@outlook.com>
2023-08-27 07:27:36 +0000

    M nvim/.config/nvim/lua/custom/utils.lua

--- nvim/.config/nvim/lua/custom/utils.lua
+++ nvim/.config/nvim/lua/custom/utils.lua
@@ -36,7 +36,7 @@ function M.isParentPath(parentPath, childPath)
   return true -- Parent path is the parent of the child path
 end
 
-local function reload_theme(name)
+local function M.reload_theme(name)
   vim.g.nvchad_theme = name
   require("base46").load_all_highlights()
   vim.api.nvim_exec_autocmds("User", { pattern = "NvChadThemeReload" })
