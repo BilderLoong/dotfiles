50098876fa92b9ce0ec7402485e667ac4e5c5001
bilder <cokoryu@outlook.com>
2023-08-27 06:07:21 +0000

    M nvim/.config/nvim/lua/custom/autocmds.lua

--- nvim/.config/nvim/lua/custom/autocmds.lua
+++ nvim/.config/nvim/lua/custom/autocmds.lua
@@ -12,11 +12,16 @@ api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
 })
 
 function disableAS()
-	vim.fn.stdpath("config")
+	local cwd = vim.loop.cmd()
+	local config_dir = vim.fn.stdpath("config")
+
+	if cwd == config_dir then
+		vim.g.auto_save = 0
+	end
 end
 
 if vim.g.vim_did_enter then
-	vim.g.auto_save = 0
+  disableAS()
 else
 	api.nvim_create_autocmd({ "Dirchanged", "VimEnter" }, {})
 end
