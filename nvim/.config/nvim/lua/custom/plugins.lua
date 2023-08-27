9a6a74edd076ffb4e1805bbbff2e01cd8a0b2512
bilder <cokoryu@outlook.com>
2023-08-27 05:55:06 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -380,7 +380,11 @@ local plugins = {
     "nvimdev/lspsaga.nvim",
     event = "LspAttach",
     config = function()
-      require("lspsaga").setup {}
+      require("lspsaga").setup {
+        symbol_in_winbar = {
+          
+        }
+      }
     end,
     dependencies = {
       "nvim-treesitter/nvim-treesitter", -- optional
