cd45558f9279bd1976249a18637833f320049939
bilder <cokoryu@outlook.com>
2023-08-27 06:43:10 +0000

    M nvim/.config/nvim/lua/custom/utils.lua

--- nvim/.config/nvim/lua/custom/utils.lua
+++ nvim/.config/nvim/lua/custom/utils.lua
@@ -1,11 +1,39 @@
 local M = {}
 
 function M.getTableKeys(tab)
-	local keyset = {}
-	for k, _ in pairs(tab) do
-		keyset[#keyset + 1] = k
-	end
-	return keyset
+  local keyset = {}
+  for k, _ in pairs(tab) do
+    keyset[#keyset + 1] = k
+  end
+  return keyset
+end
+
+function M.isParentPath(parentPath, childPath)
+  local parentComponents = {}
+  local childComponents = {}
+
+  -- Split the parent path into its components
+  for component in parentPath:gmatch "[^/]+" do
+    table.insert(parentComponents, component)
+  end
+
+  -- Split the child path into its components
+  for component in childPath:gmatch "[^/]+" do
+    table.insert(childComponents, component)
+  end
+
+  -- Check if the parent path is a parent of the child path
+  if #parentComponents > #childComponents then
+    return false -- Parent path has more components, so it can't be the parent
+  end
+
+  for i, component in ipairs(parentComponents) do
+    if component ~= childComponents[i] then
+      return false -- Components don't match, so it's not the parent
+    end
+  end
+
+  return true -- Parent path is the parent of the child path
 end
 
 return M
