b523cb55aa6ceca349889c0e3f9d265aac7c83df
bilder <cokoryu@outlook.com>
2023-08-27 06:48:08 +0000

    M nvim/.config/nvim/lua/custom/autocmds.lua

--- nvim/.config/nvim/lua/custom/autocmds.lua
+++ nvim/.config/nvim/lua/custom/autocmds.lua
@@ -16,7 +16,8 @@ api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
 function disable_auto_save()
   local cur_buf_name = vim.api.nvim_buf_get_name(0)
   local config_dir = vim.fn.stdpath "config"
-  if utils.isParentPath(config_dir,)
+  if utils.isParentPath(config_dir,cur_buf_name)
+    
   vim.g.auto_save = 0
 
   print('disable_auto_save')
