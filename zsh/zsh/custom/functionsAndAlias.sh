deca7b936fea0dd224f216b0d5eeb1f6ea51d77f
huzilong <huzilong@meituan.com>
2023-09-01 14:03:28 +0800

    M zsh/zsh/custom/functionsAndAlias.sh

--- zsh/zsh/custom/functionsAndAlias.sh
+++ zsh/zsh/custom/functionsAndAlias.sh
@@ -6,20 +6,20 @@ alias lv="lvim"
 alias gwl="git worktree list"
 alias cd='z'
 
-proxy (){
-  port=$1
+proxy() {
+	port=$1
 
-	export proxy_address="http://127.0.0.1:${port:-7890}" 
+	export proxy_address="http://127.0.0.1:${port:-7890}"
 	export all_proxy=$proxy_address
 	export http_proxy=$proxy_address
 	export https_proxy=$proxy_address
 }
 
 unproxy() {
-  unset proxy_address
-  unset all_proxy
-  unset http_proxy
-  unset https_proxy
+	unset proxy_address
+	unset all_proxy
+	unset http_proxy
+	unset https_proxy
 }
 # Git
 
@@ -30,27 +30,26 @@ unproxy() {
 # Example usage:
 # clone_my_repo my-repo /path/to/destination
 clone_my_repo() {
-  # Check if the required arguments are provided
-  if [ -z "$1" ]; then
-    echo "Please provide repo name"
-    return 1
-  fi
+	# Check if the required arguments are provided
+	if [ -z "$1" ]; then
+		echo "Please provide repo name"
+		return 1
+	fi
 
-  repo_name=$1
-  dist_path=$2
+	repo_name=$1
+	dist_path=$2
 
-  git clone "git@github.com:BilderLoong/${repo_name}.git" "$dist_path"
+	git clone "git@github.com:BilderLoong/${repo_name}.git" "$dist_path"
 }
 
-
-
+alias pbc='pbcopy'
+alias pbp='pbpaste'
 
 alias gsave='git add . && git commit -m "save" && git pull origin HEAD &&  git push origin HEAD'
 
-
 # rm_all_nest_node_modules
-trash_all_nest_node_modules () {
-  directory=${1:-.}
-  echo "removing ${directory}"
-  find $1 -name 'node_modules' -type d -prune -exec trash -rf '{}' +
+trash_all_nest_node_modules() {
+	directory=${1:-.}
+	echo "removing ${directory}"
+	find $1 -name 'node_modules' -type d -prune -exec trash -rf '{}' +
 }
