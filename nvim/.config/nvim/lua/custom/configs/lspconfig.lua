d47caaac42e900c3ed26762e73b6cb1e2a221c28
bilder <cokoryu@outlook.com>
2023-09-02 13:28:43 +0000

    M nvim/.config/nvim/lua/custom/configs/lspconfig.lua

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,32 +1,32 @@
-local configs = require "plugins.configs.lspconfig"
-local lspconfig = require "lspconfig"
+local configs = require("plugins.configs.lspconfig")
+local lspconfig = require("lspconfig")
 
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
-local servers = { "html", "cssls", "clangd", "tsserver", "pyright", "bashls", "gopls", "jsonls" }
+local servers = { "html", "cssls", "clangd", "tsserver", "pyright", "bashls", "gopls", "jsonls", "hls" }
 
 -- https://ast-grep.github.io/guide/editor-integration.html#nvim-lspconfig
 configs.ast_grep = {
-  default_config = {
-    cmd = { "sg", "lsp" },
-    -- filetypes = {'typescript'};
-    single_file_support = true,
-    root_dir = lspconfig.util.root_pattern(".git", "sgconfig.yml"),
-  },
+	default_config = {
+		cmd = { "sg", "lsp" },
+		-- filetypes = {'typescript'};
+		single_file_support = true,
+		root_dir = lspconfig.util.root_pattern(".git", "sgconfig.yml"),
+	},
 }
 -- https://github.com/b0o/SchemaStore.nvim#usage
-configs.jsonls =  {
-  settings = {
-    json = {
-      schemas = require("schemastore").json.schemas(),
-      validate = { enable = true },
-    },
-  },
+configs.jsonls = {
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas(),
+			validate = { enable = true },
+		},
+	},
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
