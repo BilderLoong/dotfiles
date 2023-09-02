2947f1af5f9e1bcbebc87719e5631ff1511f8e0b
bilder <cokoryu@outlook.com>
2023-09-02 09:29:14 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -417,6 +417,7 @@ local plugins = {
       }
     end,
     config = function(_, opts)
+      vim.print()
       require("dap-vscode-js").setup(opts)
       for _, language in ipairs { "typescript", "javascript" } do
         require("dap").configurations[language] = {
