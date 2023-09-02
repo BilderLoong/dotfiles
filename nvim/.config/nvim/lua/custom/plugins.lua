d4f885544412835fdeee8c6c1322e574c8b91583
bilder <cokoryu@outlook.com>
2023-09-02 09:36:58 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -417,7 +417,7 @@ local plugins = {
       }
     end,
     config = function(_, opts)
-      vim.pretty_print('123')
+      vim.print('Can\'t be printed.')
       require("dap-vscode-js").setup(opts)
       for _, language in ipairs { "typescript", "javascript" } do
         print('asdfasd')
