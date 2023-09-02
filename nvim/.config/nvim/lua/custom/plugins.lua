bf1078d178a8935054788ea7f042588a1c5f17d7
bilder <cokoryu@outlook.com>
2023-09-02 13:32:19 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -518,6 +518,21 @@ local plugins = {
 		filetype = { "json", "jsonc" },
 		dependencies = { "neovim/nvim-lspconfig" },
 	},
+
+  {
+  'mrcjkb/haskell-tools.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim', -- Optional
+  },
+  branch = '2.x.x', -- Recommended
+  init = function() -- Optional, see Advanced configuration
+    vim.g.haskell_tools = {
+      -- ...
+    },
+  end,
+  ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
+}
 }
 
 return plugins
