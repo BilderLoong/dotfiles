1a4228ce3a3d4db42518180738f2c4d1a087a4f5
bilder <cokoryu@outlook.com>
2023-09-03 06:31:25 +0000

    M nvim/.config/nvim/lua/custom/configs/lspconfig.lua

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,33 +1,34 @@
-local configs = require("plugins.configs.lspconfig")
-local lspconfig = require("lspconfig")
+local configs = require "plugins.configs.lspconfig"
+local lspconfig = require "lspconfig"
 
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 local servers = { "html", "cssls", "clangd", "tsserver", "pyright", "bashls", "gopls", "jsonls" }
 
-
 -- https://ast-grep.github.io/guide/editor-integration.html#nvim-lspconfig
 configs.ast_grep = {
-	default_config = {
-		cmd = { "sg", "lsp" },
-		-- filetypes = {'typescript'};
-		single_file_support = true,
-		root_dir = lspconfig.util.root_pattern(".git", "sgconfig.yml"),
-	},
+  default_config = {
+    cmd = { "sg", "lsp" },
+    -- filetypes = {'typescript'};
+    single_file_support = true,
+    root_dir = lspconfig.util.root_pattern(".git", "sgconfig.yml"),
+  },
 }
 -- https://github.com/b0o/SchemaStore.nvim#usage
 configs.jsonls = {
-	settings = {
-		json = {
-			schemas = require("schemastore").json.schemas(),
-			validate = { enable = true },
-		},
-	},
+  settings = {
+    json = {
+      schemas = require("schemastore").json.schemas(),
+      validate = { enable = true },
+    },
+  },
 }
 
+local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 end
