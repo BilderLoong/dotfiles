9b2a564866f705d690a7110ded1392740a6840ef
bilder <cokoryu@outlook.com>
2023-09-03 06:46:17 +0000

    M nvim/.config/nvim/lua/custom/configs/lspconfig.lua

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -31,6 +31,7 @@ configs.jsonls = {
 }
 
 local language_servers = require("lspconfig").util.available_servers()
+vim.pring(language_servers)
 for _, lsp in ipairs(language_servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
