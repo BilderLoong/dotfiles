4f7ba97bced78c81e8359f2a82aadd80eaf18657
bilder <cokoryu@outlook.com>
2023-09-03 06:42:10 +0000

    M nvim/.config/nvim/lua/custom/configs/lspconfig.lua

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local lspconfig = require "lspconfig"
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 -- local servers = { "html", "cssls", "clangd", "tsserver", "pyright", "bashls", "gopls", "jsonls" }
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+local language_servers = require("lspconfig").util.available_servers() 
 
 -- https://ast-grep.github.io/guide/editor-integration.html#nvim-lspconfig
 configs.ast_grep = {
