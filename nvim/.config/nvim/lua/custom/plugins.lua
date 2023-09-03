4c413b3644ebc4e2b466dc55962bf7cb9813963b
bilder <cokoryu@outlook.com>
2023-09-03 06:10:32 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -543,11 +543,16 @@ local plugins = {
 
 	{
 		"kevinhwang91/nvim-ufo",
-    event="BufReadPost",
+		event = "BufReadPost",
 		dependencies = "kevinhwang91/promise-async",
-    config = function (_, opts)
-       require('kevinhwang91/nvim-ufo').setup()
-    end
+		config = function(_, opts)
+			require("kevinhwang91/nvim-ufo").setup()
+
+			vim.o.foldcolumn = "1" -- '0' is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+		end,
 	},
 }
 
