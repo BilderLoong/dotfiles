29daf5e666a60c14c76d35cf714aa8db19cb0514
bilder <cokoryu@outlook.com>
2023-09-03 06:10:48 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -553,7 +553,7 @@ local plugins = {
 			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
-      
+
 		end,
 	},
 }
