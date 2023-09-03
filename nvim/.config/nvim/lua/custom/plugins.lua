b33f1b8e8e53712a2befb845b466f2f38d4a6ea6
bilder <cokoryu@outlook.com>
2023-09-03 06:10:40 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -548,10 +548,12 @@ local plugins = {
 		config = function(_, opts)
 			require("kevinhwang91/nvim-ufo").setup()
 
+      -- https://github.com/kevinhwang91/nvim-ufo#minimal-configuration
 			vim.o.foldcolumn = "1" -- '0' is not bad
 			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
+      
 		end,
 	},
 }
