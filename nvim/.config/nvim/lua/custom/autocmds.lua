7679b912680143317095af64cf7e3cd0b6667ec2
huzilong <huzilong@meituan.com>
2023-09-04 17:56:45 +0800

    M nvim/.config/nvim/lua/custom/autocmds.lua

--- nvim/.config/nvim/lua/custom/autocmds.lua
+++ nvim/.config/nvim/lua/custom/autocmds.lua
@@ -1,31 +1,31 @@
-local utils = require "custom.utils"
-
--- Auto save all buffer when buffer losing focus.
-local api = vim.api
-api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
-  pattern = "*",
-  callback = function(ctx)
-    api.nvim_command "silent! wall"
-
-    if vim.log.levels.TRACE then
-      -- vim.print("Save all buffers on blur.")
-    end
-  end,
-})
-
-local function disable_auto_save()
-  local cur_buf_name = vim.api.nvim_buf_get_name(0)
-  local config_dir = vim.fn.stdpath "config"
-  if not utils.isParentPath(config_dir, cur_buf_name) then
-    return
-  end
-
--- https://github.com/907th/vim-auto-save#enable-on-startup
-  vim.b.auto_save = 0
-end
-
-api.nvim_create_autocmd({
-  "BufEnter",
-}, {
-  callback = disable_auto_save,
-})
+-- local utils = require "custom.utils"
+--
+-- -- Auto save all buffer when buffer losing focus.
+-- local api = vim.api
+-- api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+--   pattern = "*",
+--   callback = function(ctx)
+--     api.nvim_command "silent! wall"
+--
+--     if vim.log.levels.TRACE then
+--       -- vim.print("Save all buffers on blur.")
+--     end
+--   end,
+-- })
+--
+-- local function disable_auto_save()
+--   local cur_buf_name = vim.api.nvim_buf_get_name(0)
+--   local config_dir = vim.fn.stdpath "config"
+--   if not utils.isParentPath(config_dir, cur_buf_name) then
+--     return
+--   end
+--
+-- -- https://github.com/907th/vim-auto-save#enable-on-startup
+--   vim.b.auto_save = 0
+-- end
+--
+-- api.nvim_create_autocmd({
+--   "BufEnter",
+-- }, {
+--   callback = disable_auto_save,
+-- })
