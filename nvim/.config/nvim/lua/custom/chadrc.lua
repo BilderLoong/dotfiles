c8915664e2d95d51fc44d852fc7d02488a9072af
huzilong <huzilong@meituan.com>
2023-09-07 18:44:26 +0800

    M nvim/.config/nvim/lua/custom/chadrc.lua

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,14 +1,28 @@
 ---@type ChadrcConfig
 local M = {
-  mappings = require "custom.mappings",
-  plugins = "custom.plugins",
-  ui = {
-    theme = "nord", -- default theme
-    nvdash = {
-      load_on_startup = false
-    },
-    
-  }
+	mappings = require("custom.mappings"),
+	plugins = "custom.plugins",
+	ui = {
+		theme = "nord", -- default theme
+		nvdash = {
+			load_on_startup = false,
+		},
+		changed_themes = {
+			onedark = {
+				base_16 = {
+					base00 = "#mycol",
+				},
+				base_30 = {
+					red = "#mycol",
+					white = "#mycol",
+				},
+			},
+
+			nord = {
+				-- and so on!
+			},
+		},
+	},
 }
 
 -- check core.mappings for table structure
