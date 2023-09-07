76ad56d80d72e9be57f99a499cd670c38fb94d47
huzilong <huzilong@meituan.com>
2023-09-07 18:34:36 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -15,7 +15,6 @@ opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.cmd([[ set nofoldenable ]])
 
-
 -- local function _start_tsserver()
 -- 	--local cwd = vim.loop.cmd()
 -- 	local root_dir =
@@ -41,13 +40,17 @@ vim.cmd([[ set nofoldenable ]])
 -- 	})
 -- end
 
+function FixGruvbox()
+	vim.api.nvim_set_hl(0, "DiffviewDiffAddAsDelete", { bg = "#431313" })
+	vim.api.nvim_set_hl(0, "DiffDelete", { bg = "none", fg = colors.dark2 })
+	vim.api.nvim_set_hl(0, "DiffviewDiffDelete", { bg = "none", fg = colors.dark2 })
+	vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#142a03" })
+	vim.api.nvim_set_hl(0, "DiffChange", { bg = "#3B3307" })
+	vim.api.nvim_set_hl(0, "DiffText", { bg = "#4D520D" })
+end
+FixGruvbox()
 
-  vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { bg = "#431313" })
-  vim.api.nvim_set_hl(0, 'DiffDelete', { bg = "none", fg = colors.dark2 })
-  vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = "none", fg = colors.dark2 })
-  vim.api.nvim_set_hl(0, 'DiffAdd', { bg = "#142a03" })
-  vim.api.nvim_set_hl(0, 'DiffChange', { bg = "#3B3307" })
-  vim.api.nvim_set_hl(0, 'DiffText', { bg = "#4D520D" })
+vim.api.nvim_create_autocmd("ColorScheme", { pattern = { "nord" }, callback = FixGruvbox })
 
 require("custom.autocmds")
 require("custom.custom_cmds")
