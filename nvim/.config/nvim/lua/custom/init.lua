27625833607b756cfb3a5675e4d6bf3f8fe30597
huzilong <huzilong@meituan.com>
2023-09-07 18:34:14 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -41,5 +41,13 @@ vim.cmd([[ set nofoldenable ]])
 -- 	})
 -- end
 
+
+  vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { bg = "#431313" })
+  vim.api.nvim_set_hl(0, 'DiffDelete', { bg = "none", fg = colors.dark2 })
+  vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = "none", fg = colors.dark2 })
+  vim.api.nvim_set_hl(0, 'DiffAdd', { bg = "#142a03" })
+  vim.api.nvim_set_hl(0, 'DiffChange', { bg = "#3B3307" })
+  vim.api.nvim_set_hl(0, 'DiffText', { bg = "#4D520D" })
+
 require("custom.autocmds")
 require("custom.custom_cmds")
