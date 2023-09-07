adadcb632983fed70424522e790c321af3e51a65
huzilong <huzilong@meituan.com>
2023-09-07 18:15:41 +0800

    M nvim/.config/nvim/lua/custom/chadrc.lua

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -3,7 +3,7 @@ local M = {
   mappings = require "custom.mappings",
   plugins = "custom.plugins",
   ui = {
-    theme = "aquarium", -- default theme
+    theme = "nord", -- default theme
     nvdash = {
       load_on_startup = false
     }
