e5954899787add40e655514044a3854fbaba0a7b
huzilong <huzilong@meituan.com>
2023-09-08 11:43:18 +0800

    M nvim/.config/nvim/lua/custom/chadrc.lua

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -4,6 +4,7 @@
 -- To find any highlight groups: "<cmd> Telescope highlights"
 -- Each highlight group can take a table with variables fg, bg, bold, italic, etc
 -- base30 variable names can also be used as colors
+
 ---@type ChadrcConfig
 local M = {
   mappings = require "custom.mappings",
