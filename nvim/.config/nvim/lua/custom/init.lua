7ea308140b4b688c185f3227750a23697f1b5f24
huzilong <huzilong@meituan.com>
2023-09-08 11:42:39 +0800

    M nvim/.config/nvim/lua/custom/init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -40,6 +40,13 @@ vim.cmd([[ set nofoldenable ]])
 -- 	})
 -- end
 --
+--  DiffChange = { fg = "none", bg = "#272D43" },
+  DiffText = { fg = "none", bg = "#394b70" },
+  DiffDelete = { fg = "none", bg = "#3F2D3D" },
+  DiffviewDiffAddAsDelete = { fg = "none", bg = "#3f2d3d" },
+  DiffviewDiffDelete = { fg = "none", bg = "#3B4252" },
+  -- Left pannel
+  DiffAddAsDelete = { fg = "none", bg = "#3F2D3D" },
 --
 --
 -- GitSignsAddNr                          { GitSignsAdd }, -- GitSignsAddNr  
