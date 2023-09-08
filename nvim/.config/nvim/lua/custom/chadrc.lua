b18a4c3a727ee1dd7e3e05819b848188c4d83c45
huzilong <huzilong@meituan.com>
2023-09-08 11:43:22 +0800

    M nvim/.config/nvim/lua/custom/chadrc.lua

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,9 +1,10 @@
--- local theme = require("base46").get_theme_tb "base_16"
--- local colors = require("base46").get_theme_tb "base_30"
 
 -- To find any highlight groups: "<cmd> Telescope highlights"
 -- Each highlight group can take a table with variables fg, bg, bold, italic, etc
 -- base30 variable names can also be used as colors
+
+-- local theme = require("base46").get_theme_tb "base_16"
+-- local colors = require("base46").get_theme_tb "base_30"
 
 ---@type ChadrcConfig
 local M = {
