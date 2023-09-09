e143eb858c1c78dd0f5dd27aa418a217fed5db68
bilder <cokoryu@outlook.com>
2023-09-09 07:56:26 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -458,7 +458,8 @@ local plugins = {
             name = "Launch current file in new node process(" .. language .. ")",
             program = "${file}",
             cwd = "${workspaceFolder}",
-            port = 8123
+            port = 8123,
+            
           },
         }
       end
