8fb342d643deac4fd69235fd1e6e8c1d7a47f291
bilder <cokoryu@outlook.com>
2023-09-09 07:58:02 +0000

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -455,7 +455,7 @@ local plugins = {
             -- launch a new process to attach the debugger to
             request = "launch",
             -- name of the debug action you have to select for this config
-            name = "Launch current file in new node process(" .. language .. ")",
+            name = "Launch current file in new node process (" .. language .. ")",
             program = "${file}",
             cwd = "${workspaceFolder}",
             port = 8123,
