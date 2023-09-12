87e7a14772cb7758cb81f80452e77f21eb50260a
huzilong <huzilong@meituan.com>
2023-09-12 16:33:10 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -430,8 +430,8 @@ local plugins = {
 		dependencies = { "mfussenegger/nvim-dap" },
 		opts = function(_, default_nvchad_opts)
 			return {
-				debugger_path = vim.fn.stdpath "data" .. "/mason/packages/js-debug-adapter/",
-				debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+				debugger_path = vim.fn.stdpath "data" .. "/mason/packages/js-debug-adapter",
+				-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
 				adapters = {
 					"pwa-node",
 					"pwa-chrome",
