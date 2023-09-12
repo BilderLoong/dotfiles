61114ff903089a13bd8da6d2500a2f1878f44c42
huzilong <huzilong@meituan.com>
2023-09-12 11:41:11 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -45,7 +45,7 @@ zinit wait lucid for \
     has'git' OMZP::git \
     has'exa' DarrinTisdale/zsh-aliases-exa \
     zdharma-continuum/fast-syntax-highlighting \
-        has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd"  \
+        has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd" \
     zdharma-continuum/null  \
          wait'!0' has'starship' id-as'starship_init' eval"starship init zsh" \
     zdharma-continuum/null  \
