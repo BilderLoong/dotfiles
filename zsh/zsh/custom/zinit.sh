97c7788287c8b3534ece4df1d416439be2f4c805
huzilong <huzilong@meituan.com>
2023-09-12 11:36:45 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -55,7 +55,7 @@ zinit wait lucid for \
          has'navi' id-as'navi-widget' eval"navi widget zsh" \
     zdharma-continuum/null \
         has'ast-grep' id-as'sg-completions' eval"ast-grep completions"  \
-atload'zicompinit; zicdreplay'\
+        atload'zicompinit; zicdreplay'\
     zdharma-continuum/null
 
 # Fish-like autosuggestions for zsh 
