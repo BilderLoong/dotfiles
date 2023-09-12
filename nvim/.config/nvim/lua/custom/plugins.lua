627ab8767be1004695d4feba34bd27161f1b152e
huzilong <huzilong@meituan.com>
2023-09-12 16:30:43 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -441,7 +441,7 @@ local plugins = {
 					"node",
 					"chrome",
 				},
-				log_file_path = vim.fn.stdpath("log") .. "/nvim-dap-vscode-js", -- Path for file logging
+				-- log_file_path = vim.fn.stdpath("log") .. "/nvim-dap-vscode-js", -- Path for file logging
 				-- log_file_level = vim.log.levels.TRACE, -- Logging level for output to file. Set to false to disable file logging.
 			}
 		end,
