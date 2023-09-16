0eb44f221adcf5b3588056deb1898cade950cf1d
bilder <cokoryu@outlook.com>
2023-09-16 17:17:49 +0800

    M nvim/.config/nvim/lua/custom/playground.lua

--- nvim/.config/nvim/lua/custom/playground.lua
+++ nvim/.config/nvim/lua/custom/playground.lua
@@ -6,7 +6,7 @@ function M.start_tsserver()
 	local root_dir =
 		vim.fs.dirname(vim.fs.find({ "tsconfig.json", "package.json", "jsconfig.json", ".git" }, { upward = true })[1])
 
-	-- vim.print("root_dir", root_dir)
+	vim.print("root_dir", root_dir)
 
   -- Don't use `vim.lsp.start` since it will attach the server to the curretn buffer.
 	local client_id = vim.lsp.start_client({
