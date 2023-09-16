e8d988735ab907a6d4098f5482815b9b79ae44c1
bilder <cokoryu@outlook.com>
2023-09-16 16:03:34 +0800

    ?? nvim/.config/nvim/lua/custom/playground.lua

---
+++ nvim/.config/nvim/lua/custom/playground.lua
@@ -0,0 +1,13 @@
+local function _start_tsserver()
+	--local cwd = vim.loop.cmd()
+	local root_dir =
+		vim.fs.dirname(vim.fs.find({ "tsconfig.json", "package.json", "jsconfig.json", ".git" }, { upward = true })[1])
+
+	vim.print("root_dir", root_dir)
+
+	local client = vim.lsp.start({
+		name = "tsserver",
+		cmd = { "typescript-language-server", "--stdio" },
+		root_dir = root_dir,
+	})
+end
