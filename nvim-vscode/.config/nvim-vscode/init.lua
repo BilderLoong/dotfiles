625f1a83668a771990eb324a75593b54e77c7f99
huzilong <huzilong@meituan.com>
2023-09-19 17:25:05 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.print('lazypath',)
+vim.print('lazypath',lazypath)
 -- if not vim.loop.fs_stat(lazypath) then
 --   vim.fn.system({
 --     "git",
