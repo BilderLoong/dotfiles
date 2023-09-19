d6230a6e26590c64fe9f31caf252887396181951
huzilong <huzilong@meituan.com>
2023-09-19 17:24:19 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -11,3 +11,18 @@ vim.cmd([[
   xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
   nnoremap <Leader>ca <Cmd>call VSCodeNotify('editor.action.quickFix')<CR> " Replace the default quickfix into code action.
 ]])
+
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
