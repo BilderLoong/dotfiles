a56a5aed7390f55f176a41de110dc830f6dea8ac
huzilong <huzilong@meituan.com>
2023-09-19 17:17:11 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -1,3 +1,6 @@
-vim.cmd([[]])
+vim.cmd([[
+
 nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
 xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
+
+]])
