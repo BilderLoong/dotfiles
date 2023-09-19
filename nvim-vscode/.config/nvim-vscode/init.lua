6ff8f39be9de56b7addf7107387c00fb4ec30c46
huzilong <huzilong@meituan.com>
2023-09-19 18:37:41 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -87,11 +87,11 @@ vim.g.mapleader = " "
 -- 	},
 -- })
 
-vim.cmd([[
-  nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
-  xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
-  nnoremap <Leader>ca <Cmd>call VSCodeNotify('editor.action.quickFix')<CR> " Replace the default quickfix into code action.
-]])
+-- vim.cmd([[
+--   nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
+--   xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
+--   nnoremap <Leader>ca <Cmd>call VSCodeNotify('editor.action.quickFix')<CR> " Replace the default quickfix into code action.
+-- ]])
 
 vim.keymap.set("n", "gr", function()
 	require("vscode-neovim").notify("editor.action.referenceSearch.trigger")
