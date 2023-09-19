8f2d97c3fb9d94702823f150d60e02063004d927
huzilong <huzilong@meituan.com>
2023-09-19 18:19:43 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -88,8 +88,6 @@ require("lazy").setup({
 	},
 })
 
-vim.print(require("vscode-neovim"))
-
 vim.cmd([[
   nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
   xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
