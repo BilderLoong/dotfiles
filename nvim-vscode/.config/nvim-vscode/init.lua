541f4b8b91fcb93de9ba7b311f68a458524e7f92
huzilong <huzilong@meituan.com>
2023-09-19 17:57:20 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -87,3 +87,11 @@ require("lazy").setup({
 		},
 	},
 })
+
+-- nnoremap <Leader>o o<Esc>
+
+vim.cmd([[
+  nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
+  xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
+  nnoremap <Leader>ca <Cmd>call VSCodeNotify('editor.action.quickFix')<CR> " Replace the default quickfix into code action.
+]])
