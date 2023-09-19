28ef99c2fa434e1d32ae108bf44da76bf38803df
huzilong <huzilong@meituan.com>
2023-09-19 17:20:37 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -1,6 +1,7 @@
 
 vim.opt.mapleader = "<Space>"
 
+
 vim.cmd([[
   nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
   xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
