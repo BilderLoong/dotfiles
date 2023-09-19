a88ece686b599f7bdd7b1e1384dc97fbd5592861
huzilong <huzilong@meituan.com>
2023-09-19 17:27:52 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     "unblevable/quick-scope",
     event = "BufReadPost",
     config = function()
+
       vim.cmd [[
 			  highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
 			  highlight QuickScopeSecondary guifg='#6fffff' gui=underline ctermfg=81 cterm=underline
