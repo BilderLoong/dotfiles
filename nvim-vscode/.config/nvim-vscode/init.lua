3736d93cbdba25fb050db72f6bf7c0bfd98baab7
huzilong <huzilong@meituan.com>
2023-09-19 18:53:41 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -1,92 +1,92 @@
 vim.g.mapleader = " "
 
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
--- 	vim.fn.system({
--- 		"git",
--- 		"clone",
--- 		"--filter=blob:none",
--- 		"https://github.com/folke/lazy.nvim.git",
--- 		"--branch=stable", -- latest stable release
--- 		lazypath,
--- 	})
--- end
--- vim.opt.rtp:prepend(lazypath)
-
----@type NvPluginSpec[]
--- require("lazy").setup({
--- 	-- {
--- 	-- 	"unblevable/quick-scope",
--- 	-- 	event = "BufReadPost",
--- 	-- 	config = function()
--- 	-- 		vim.cmd([[
--- 	-- 		  highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
--- 	-- 		  highlight QuickScopeSecondary guifg='#6fffff' gui=underline ctermfg=81 cterm=underline
---  --   ]])
--- 	-- 	end,
--- 	-- },
--- 	--
--- 	-- {
--- 	-- 	"kylechui/nvim-surround",
--- 	-- 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
--- 	-- 	keys = { "c", "y", "d" },
--- 	-- 	config = function()
--- 	-- 		require("nvim-surround").setup({
--- 	-- 			-- Configuration here, or leave empty to use defaults
--- 	-- 		})
--- 	-- 	end,
--- 	-- },
--- 	--
--- 	-- {
--- 	-- 	"wellle/targets.vim",
--- 	-- 	event = "BufReadPost",
--- 	-- },
--- 	--
--- 	-- {
--- 	-- 	"ggandor/lightspeed.nvim",
--- 	-- 	event = "BufReadPost",
--- 	-- },
--- }, {
--- 	performance = {
+-- -- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- -- if not vim.loop.fs_stat(lazypath) then
+-- -- 	vim.fn.system({
+-- -- 		"git",
+-- -- 		"clone",
+-- -- 		"--filter=blob:none",
+-- -- 		"https://github.com/folke/lazy.nvim.git",
+-- -- 		"--branch=stable", -- latest stable release
+-- -- 		lazypath,
+-- -- 	})
+-- -- end
+-- -- vim.opt.rtp:prepend(lazypath)
 --
--- 		defaults = {
--- 			lazy = true, -- should plugins be lazy-loaded?
--- 		},
+-- ---@type NvPluginSpec[]
+-- -- require("lazy").setup({
+-- -- 	-- {
+-- -- 	-- 	"unblevable/quick-scope",
+-- -- 	-- 	event = "BufReadPost",
+-- -- 	-- 	config = function()
+-- -- 	-- 		vim.cmd([[
+-- -- 	-- 		  highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+-- -- 	-- 		  highlight QuickScopeSecondary guifg='#6fffff' gui=underline ctermfg=81 cterm=underline
+-- --  --   ]])
+-- -- 	-- 	end,
+-- -- 	-- },
+-- -- 	--
+-- -- 	-- {
+-- -- 	-- 	"kylechui/nvim-surround",
+-- -- 	-- 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+-- -- 	-- 	keys = { "c", "y", "d" },
+-- -- 	-- 	config = function()
+-- -- 	-- 		require("nvim-surround").setup({
+-- -- 	-- 			-- Configuration here, or leave empty to use defaults
+-- -- 	-- 		})
+-- -- 	-- 	end,
+-- -- 	-- },
+-- -- 	--
+-- -- 	-- {
+-- -- 	-- 	"wellle/targets.vim",
+-- -- 	-- 	event = "BufReadPost",
+-- -- 	-- },
+-- -- 	--
+-- -- 	-- {
+-- -- 	-- 	"ggandor/lightspeed.nvim",
+-- -- 	-- 	event = "BufReadPost",
+-- -- 	-- },
+-- -- }, {
+-- -- 	performance = {
+-- --
+-- -- 		defaults = {
+-- -- 			lazy = true, -- should plugins be lazy-loaded?
+-- -- 		},
+-- --
+-- -- 		rtp = {
+-- -- 			disabled_plugins = {
+-- -- 				"2html_plugin",
+-- -- 				"tohtml",
+-- -- 				"getscript",
+-- -- 				"getscriptPlugin",
+-- -- 				"gzip",
+-- -- 				"logipat",
+-- -- 				"netrw",
+-- -- 				"netrwPlugin",
+-- -- 				"netrwSettings",
+-- -- 				"netrwFileHandlers",
+-- -- 				"matchit",
+-- -- 				"tar",
+-- -- 				"tarPlugin",
+-- -- 				"rrhelper",
+-- -- 				"spellfile_plugin",
+-- -- 				"vimball",
+-- -- 				"vimballPlugin",
+-- -- 				"zip",
+-- -- 				"zipPlugin",
+-- -- 				"tutor",
+-- -- 				"rplugin",
+-- -- 				"syntax",
+-- -- 				"synmenu",
+-- -- 				"optwin",
+-- -- 				"compiler",
+-- -- 				"bugreport",
+-- -- 				"ftplugin",
+-- -- 			},
+-- -- 		},
+-- -- 	},
+-- -- })
 --
--- 		rtp = {
--- 			disabled_plugins = {
--- 				"2html_plugin",
--- 				"tohtml",
--- 				"getscript",
--- 				"getscriptPlugin",
--- 				"gzip",
--- 				"logipat",
--- 				"netrw",
--- 				"netrwPlugin",
--- 				"netrwSettings",
--- 				"netrwFileHandlers",
--- 				"matchit",
--- 				"tar",
--- 				"tarPlugin",
--- 				"rrhelper",
--- 				"spellfile_plugin",
--- 				"vimball",
--- 				"vimballPlugin",
--- 				"zip",
--- 				"zipPlugin",
--- 				"tutor",
--- 				"rplugin",
--- 				"syntax",
--- 				"synmenu",
--- 				"optwin",
--- 				"compiler",
--- 				"bugreport",
--- 				"ftplugin",
--- 			},
--- 		},
--- 	},
--- })
-
 -- vim.cmd([[
 --   nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
 --   xnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
