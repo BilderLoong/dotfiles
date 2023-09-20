4b1930e4ad77888b440dce135dc9e2d72ddd4bab
huzilong <huzilong@meituan.com>
2023-09-20 11:36:49 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -164,7 +164,20 @@ local function plugins()
 			dependencies = "nvim-treesitter/nvim-treesitter",
 		},
 
-		vim due to dependencies of LSP: https://github.com/mfussenegger/nvim-treehopper/blob/5a28bff46c05d28bdb4bcaef67e046eb915a9390/lua/tsht.lua#L96
+		{
+			"folke/flash.nvim",
+			event = "BufReadPost",
+			---@type Flash.Config
+			opts = {},
+      keys = {
+        { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+        { "m", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
+		},
+		-- Doesn't in VSCodeNeovim due to dependencies of LSP: https://github.com/mfussenegger/nvim-treehopper/blob/5a28bff46c05d28bdb4bcaef67e046eb915a9390/lua/tsht.lua#L96
 		--[[ {
 			"mfussenegger/nvim-treehopper",
 			dependencies = "nvim-treesitter/nvim-treesitter",
