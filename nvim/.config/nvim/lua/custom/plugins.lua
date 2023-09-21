e7bfe956b27f2cfd787af58d5a1ee2b6c2fc164f
huzilong <huzilong@meituan.com>
2023-09-21 11:07:00 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -435,8 +435,11 @@ local plugins = {
     event = "LspAttach",
     config = function()
       require("lspsaga").setup {
-        keys = {
-          vsplit = "v",
+        finder = {
+          max_height = 0.6,
+          keys = {
+            vsplit = "v",
+          },
         },
       }
     end,
