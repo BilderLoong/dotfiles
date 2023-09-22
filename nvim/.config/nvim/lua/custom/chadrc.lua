398a36b633becdf77ac4d6f480f041cc5a239d0a
huzilong <huzilong@meituan.com>
2023-09-22 11:53:02 +0800

    M nvim/.config/nvim/lua/custom/chadrc.lua

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -2,41 +2,41 @@
 -- Each highlight group can take a table with variables fg, bg, bold, italic, etc
 -- base30 variable names can also be used as colors--- @type ChadrcConfig
 local M = {
-  mappings = require "custom.mappings",
-  plugins = "custom.plugins",
+	mappings = require("custom.mappings"),
+	plugins = "custom.plugins",
 
-  ui = {
-    theme = "nord", -- default theme
-    nvdash = {
-      load_on_startup = false,
-    },
+	ui = {
+		theme = "nord", -- default theme
+		nvdash = {
+			load_on_startup = false,
+		},
 
-    hl_override = {
-      DiffAdd = {
-        bg = { "green", -25 },
-        fg = "NONE",
-      },
+		hl_override = {
+			DiffAdd = {
+				bg = { "green", -25 },
+				fg = "NONE",
+			},
 
-      DiffDelete = {
-        bg = { "red", -2 },
-        fg = "NONE",
-      },
+			DiffDelete = {
+				bg = { "red", -2 },
+				fg = "NONE",
+			},
 
-      DiffText = {
-        bg = { "yellow", -30 },
-        fg = "NONE",
-      },
+			DiffText = {
+				bg = { "yellow", -30 },
+				fg = "NONE",
+			},
 
-      DiffChange = {
-        bg = { "yellow", -40 },
-        fg = "NONE",
-      },
-    },
+			DiffChange = {
+				bg = { "yellow", -40 },
+				fg = "NONE",
+			},
+		},
 
-    statusline = {
-overriden_modules =   modules[5]
-    }
-  },
+		statusline = {
+			overriden_modules = modules[5],
+		},
+	},
 }
 
 -- lua require("base46").load_all_highlights()
