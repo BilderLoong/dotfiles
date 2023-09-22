8365773f366a30ad0b1a7b43bd2286e4c9c9d2e1
huzilong <huzilong@meituan.com>
2023-09-22 11:43:42 +0800

    M nvim/.config/nvim/lua/custom/configs/null-ls.lua

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -7,10 +7,9 @@ local ca = null_ls.builtins.code_actions
 local sources = {
   -- web developement
   fmt.prettier.with {
-  milliseconds
-  timeout = 10000,
+  milliseconds timeout = 10000,
   },
-  fmt.prettierd,
+  -- fmt.prettierd,
   ca.eslint_d.with {
     method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
   },
