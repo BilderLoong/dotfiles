971bea828437422dd8f5267b4000dedf793bd1c1
huzilong <huzilong@meituan.com>
2023-10-07 15:19:19 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -11,7 +11,8 @@ vim.keymap.set(
 	{ "n", "v", "x" },
 	"j",
 	function()
-    vim.print('count',vim.v.count,)
+		vim.print("count", vim.v.count)
+		vim.print("mode", vim.fn.mode(1))
 		return (vim.v.count ~= 0 or string.sub(vim.fn.mode(1), 1, 2) == "no") and "j" or "gj"
 	end,
 	{ expr = true, desc = "Move up" }
