e9f61415fb7f8a236d9ba56df42b1a6b93f82525
huzilong <huzilong@meituan.com>
2023-10-07 09:56:25 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -39,7 +39,8 @@ local plugins = {
     opts = {},
       -- stylua: ignore
       keys = {
-        { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+        { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash Jump" },
+        { "z", mode = {  "o"  }, function() require("flash").jump() end, desc = "Flash Jump" },
         { "m", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
         { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
