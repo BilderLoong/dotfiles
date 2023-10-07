5b3dd31c7b34c26bd4d5bd1ca7488c5471df2ec9
huzilong <huzilong@meituan.com>
2023-10-07 15:24:42 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -22,7 +22,7 @@ vim.keymap.set(
 vim.keymap.set(
 	{ "n", "v", "x" },
 	"k",
-	'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
+	[ v:count || mode(1)[0:1] == "no" ? "k" : "gk" ],
 	[[v:count || mode(1)[0:1] == "no" ? "j" : "gj"]],
 	{ expr = true, silent = true, desc = "Move down" }
 )
