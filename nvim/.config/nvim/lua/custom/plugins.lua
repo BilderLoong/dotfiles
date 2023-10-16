b2a55695467d23b59f5ceeda2bac7d23f26353b8
huzilong <huzilong@meituan.com>
2023-10-16 11:14:32 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -438,6 +438,7 @@ local plugins = {
       require("lspsaga").setup {
         finder = {
           max_height = 0.6,
+          
           keys = {
             vsplit = "v",
             split = "s",
