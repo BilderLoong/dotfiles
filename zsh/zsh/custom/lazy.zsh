cccb6a6d2fdc5d61b32fffe625ca4f775455fb60
huzilong <huzilong@meituan.com>
2023-11-27 12:00:09 +0800

    M zsh/zsh/custom/lazy.zsh

--- zsh/zsh/custom/lazy.zsh
+++ zsh/zsh/custom/lazy.zsh
@@ -1,4 +1,8 @@
 source "$ZSH_CUSTOM/functionsAndAlias.sh"
+# https://stackoverflow.com/a/54618022/11602758
+if [[ $(uname) == "Darwin" ]]; then
+	source "$ZSH_CUSTOM/macOS.sh"
+fi
 
 # bun completions
 [ -s "/home/birudo/.bun/_bun" ] && source "/home/birudo/.bunajjj/_bun"
