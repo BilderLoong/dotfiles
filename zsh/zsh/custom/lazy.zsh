49c560d221fc4370b7f0600618a817cc85bf2512
huzilong <huzilong@meituan.com>
2023-11-27 11:55:32 +0800

    M zsh/zsh/custom/lazy.zsh

--- zsh/zsh/custom/lazy.zsh
+++ zsh/zsh/custom/lazy.zsh
@@ -1,2 +1,5 @@
+# bun completions
+[ -s "/home/birudo/.bun/_bun" ] && source "/home/birudo/.bun/_bun"
+
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
