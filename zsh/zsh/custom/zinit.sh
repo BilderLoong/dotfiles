71b56ed7d32ceb137af27763b3ae7b97469ba022
huzilong <huzilong@meituan.com>
2023-11-27 12:01:15 +0800

    M zsh/zsh/custom/zinit.sh

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -67,7 +67,6 @@ zinit wait"1" lucid for \
     id-as"pyenv_init" \
     atinit'export PYENV_ROOT="$HOME/.pyenv"; export PATH="$PYENV_ROOT/bin:$PATH"' \
   zdharma-continuum/null \
-    # For async source local file.
     id-as"local_async_file" \
     atinit"source $ZSH_CUSTOM/lazy.zsh" \
   zdharma-continuum/null 
