dbd5dea0281fd4f46d4886e0414615d437806319
huzilong <huzilong@meituan.com>
2024-01-05 11:37:36 +0800

    M zsh/zsh/custom/lazy/zle.sh

--- zsh/zsh/custom/lazy/zle.sh
+++ zsh/zsh/custom/lazy/zle.sh
@@ -1,4 +1,4 @@
-
+# https://zenn.dev/nokogiri/articles/ec99e40df54555#%5Er-%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E5%B1%A5%E6%AD%B4%E3%82%92%E6%A4%9C%E7%B4%A2
 function fzf-select-history() {
 	BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
 	CURSOR=$#BUFFER
