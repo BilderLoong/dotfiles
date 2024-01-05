18a3ae4c9dba39a2f3d53f002555bb8fee764084
huzilong <huzilong@meituan.com>
2024-01-05 10:54:38 +0800

    M zsh/zsh/custom/zle.sh

--- zsh/zsh/custom/zle.sh
+++ zsh/zsh/custom/zle.sh
@@ -1,8 +1,7 @@
-
 function fzf-select-history() {
-    BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
-    CURSOR=$#BUFFER
-    zle reset-prompt
+	BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
+	CURSOR=$#BUFFER
+	zle reset-prompt
 }
 zle -N fzf-select-history
 bindkey '^r' fzf-select-history
