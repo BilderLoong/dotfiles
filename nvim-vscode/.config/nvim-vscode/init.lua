dcbd9a47e46911577e429ddf0432d759bfba765d
huzilong <huzilong@meituan.com>
2024-01-19 11:51:34 +0800

    M nvim-vscode/.config/nvim-vscode/init.lua

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -343,6 +343,7 @@ local function vscode_ui()
 		-- vscode_neovim.notify("workbench.action.focusSideBar")
 	end)
 
+	--  Toggle Primary Sidebar.
 	keymap.set({ "n", "x", "i" }, "<C-n>", function()
 		vscode_neovim.action("workbench.action.toggleSidebarVisibility")
 		-- vscode_neovim.notify("workbench.action.focusSideBar")
