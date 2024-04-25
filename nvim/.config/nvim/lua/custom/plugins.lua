e4faaeb43d33b9fca540729a120568bc9fd790d3
huzilong <huzilong@meituan.com>
2024-04-25 10:51:18 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -758,10 +758,6 @@ local plugins = {
       {
         "williamboman/mason.nvim",
         opts = {
-          registries = {
-            "github:nvim-java/mason-registry",
-            "github:mason-org/mason-registry",
-          },
         },
       },
     },
