11dd8536133dc4069bbffa10a428790b05110539
huzilong <huzilong@meituan.com>
2024-04-25 10:52:38 +0800

    M nvim/.config/nvim/lua/custom/configs/lspconfig.lua

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -24,7 +24,7 @@ local servers = {
   "kotlin_language_server",
 }
 
--- 
+-- https://github.com/nvim-java/nvim-java?tab=readme-ov-file#hammer-how-to-install
 require("java").setup() 
 
 for _, lsp in ipairs(servers) do
