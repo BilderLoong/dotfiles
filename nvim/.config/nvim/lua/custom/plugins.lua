73a300bb153d5e326c361455fe334a22dcc0347c
bilder <cokoryu@outlook.com>
2024-05-18 14:27:48 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -787,7 +787,7 @@ local plugins = {
       vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
       vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
       vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
-    end,
+    end
   },
 }
 
