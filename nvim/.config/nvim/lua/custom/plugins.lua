6947cba4cd2d4e16054a4b6665ed3a05c252adb3
bilder <cokoryu@outlook.com>
2024-05-18 14:42:44 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -21,7 +21,6 @@ local plugins = {
     ---@type Flash.Config
     opts = {},
     kes = {
-
         -- stylua: ignore start
       { "m", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash Jump" },
       { "M", mode = { "n","o",  "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
@@ -33,7 +32,7 @@ local plugins = {
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
 
-        -- stylua: ignore start
+      -- stylua: ignore end
     },
   },
   {
