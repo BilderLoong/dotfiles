7df2e2663e2e7e98d8f57ea1b2f7595f772c1d14
bilder <cokoryu@outlook.com>
2024-05-18 14:22:44 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -769,7 +769,14 @@ local plugins = {
       require("yanky").setup()
       load_mappings "yanky"
     end,
-  },
+  },{
+    "gbprod/substitute.nvim",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}
 }
 
 return plugins
