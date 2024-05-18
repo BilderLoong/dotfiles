b5fec5af126ba59d09ca15149500ef21611f3e7d
bilder <cokoryu@outlook.com>
2024-05-18 14:32:22 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -779,16 +779,14 @@ local plugins = {
       -- refer to the configuration section below
     },
     config = function(_, opts)
-      -- require("substitute").setup {
-      --   on_substitute = require("yanky.integration").substitute()
-      -- }
+      require("substitute").setup {
+        on_substitute = require("yanky.integration").substitute(),
+      }
       vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
       vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
       vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
       vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
     end,
-
-    -- end,
   },
 }
 
