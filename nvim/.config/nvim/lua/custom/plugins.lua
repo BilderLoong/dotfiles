5e26f7857056bcc47b29582e6ce5d326fd220bf0
bilder <cokoryu@outlook.com>
2024-05-18 14:39:58 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -23,6 +23,7 @@ local plugins = {
     kes = {
       { "m", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash Jump" },
       { "M", mode = { "n","o",  "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "m", mode = { "n","o",  "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       -- Use `m` and `M` instead of `s` and `S` to avoid the conflict with nvim-surround.
       -- { "m", mode = { "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
       -- { "M", mode = { "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
