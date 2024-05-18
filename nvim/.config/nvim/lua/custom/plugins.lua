74d52d860144572cc7550c698ceeadf474990e7b
bilder <cokoryu@outlook.com>
2024-05-18 14:53:01 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -793,10 +793,10 @@ local plugins = {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config=function (_,opts)
-      kjplocal harpoon = require("harpoon")
+    config = function(_, opts)
+      local harpoon = require "harpoon"
       harpoon:setup()
-    end
+    end,
   },
 }
 
