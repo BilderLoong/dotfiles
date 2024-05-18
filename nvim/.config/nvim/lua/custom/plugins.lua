03095f65dc4d9d0062266c330a626415a3182564
bilder <cokoryu@outlook.com>
2024-05-18 15:00:22 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -774,17 +774,8 @@ local plugins = {
     "gbprod/substitute.nvim",
     event = BufEnterLike,
     keys = {
-      {
-        "sx",
-        mode = { "n" },
-        function()
-          require("substitute.exchange").operator()
-        end,
-        desc = "Substitute Exchange Operator",
-        { noremap = true },
-      },
-      {
-        "sxx",
+      { "sx", mode = { "n" }, function() require("substitute.exchange").operator() end, desc = "Substitute Exchange Operator", { noremap = true }, },
+      { "sxx",
         mode = { "n" },
         function()
           require("substitute.exchange").line()
