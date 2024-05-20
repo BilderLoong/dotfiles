f4d8c30421e9785af26d49a7097f8c33f2ef163f
huzilong <huzilong@meituan.com>
2024-05-20 17:06:36 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -810,6 +810,8 @@ local plugins = {
       require("glance").setup {
         border = {
           enable = true, -- Show window borders. Only horizontal borders allowed
+          top_char = "―",
+          bottom_char = "―",
         },
       }
     end,
