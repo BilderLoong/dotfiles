e09a1963fbd6629344ad585f0387e73a83b33b00
huzilong <huzilong@meituan.com>
2024-05-28 14:32:09 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -836,7 +836,16 @@ local plugins = {
     opts = {
       -- options
     },
-  }
+  },
+  {
+    "dstein64/vim-startuptime",
+    -- lazy-load on a command
+    cmd = "StartupTime",
+    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+    init = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
   -- {
   --   "ThePrimeagen/harpoon",
   --   branch = "harpoon2",
