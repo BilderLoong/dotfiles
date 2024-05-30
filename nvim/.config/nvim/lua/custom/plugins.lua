a56c7a3a7b66b4590d3478b06645289ef8ef4727
huzilong <huzilong@meituan.com>
2024-05-30 14:24:47 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -1,4 +1,4 @@
-local BufEnterLike = { "VeryLazy" }
+local BufEnterLike = { "User LazyFilePost" }
 local User_FilePost = { "User FilePost" }
 local utils = require "custom.utils"
 local load_mappings = require("core.utils").load_mappings
