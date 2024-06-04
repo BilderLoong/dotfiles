1d1074ccdc64a7c35b8ac14489c7ea4c162f70cc
huzilong <huzilong@meituan.com>
2024-06-04 14:58:18 +0800

    M nvim/.config/nvim/lua/custom/mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -84,7 +84,7 @@ M.lspconfig = {
   n = {
     ["gd"] = {
       "<cmd> Glance type_definitions  <CR>",
-      "Glance LSP definitions",
+      "Glance LSP definitions yo~",
     },
     ["gr"] = {
       "<cmd> Glance references",
