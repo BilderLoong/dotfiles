c76771df53e29161d32f5b39b258b50e29ec952d
huzilong <huzilong@meituan.com>
2024-06-21 08:55:31 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -193,7 +193,7 @@ local plugins = {
         names = vim.tbl_filter(function(name)
           local linter = lint.linters[name]
           if not linter then
-            require('log')
+            require('custom.log')
             vim.notify("Linter not found: " .. name)
           end
           return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
