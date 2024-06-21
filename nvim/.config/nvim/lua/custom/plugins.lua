d0e713a7674843a115e7e2f71b55b5e14ad0d051
huzilong <huzilong@meituan.com>
2024-06-21 08:59:36 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -142,7 +142,7 @@ local plugins = {
     -- If you want the formatexpr, here is the place to set it
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
-}
+},
 
   {
     "mfussenegger/nvim-lint",
