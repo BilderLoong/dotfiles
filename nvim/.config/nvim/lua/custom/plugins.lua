340832e39a5189dbf6f8e9b1415b3cb8642ef736
huzilong <huzilong@meituan.com>
2024-06-21 09:09:47 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -126,7 +126,7 @@ local plugins = {
       formatters_by_ft = {
         lua = { "stylua" },
         python = { "isort", "black" },
-        javascript = { { "prettierd", "prettier" } },
+        javascript = { { "prettier" } },
       },
       -- Set up format-on-save
       format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
