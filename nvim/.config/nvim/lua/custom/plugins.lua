e3383fb20eb5affdb48a98d1b9a892fa2553e718
huzilong <huzilong@meituan.com>
2024-06-21 08:45:43 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -105,6 +105,7 @@ local plugins = {
       return { pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook() }
     end,
   },
+
   {
     "stevearc/conform.nvim",
     dependencies = { "mason.nvim" },
