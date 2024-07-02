2a153909676821cef3407f0b8ad30e6b44f67473
huzilong <huzilong@meituan.com>
2024-07-02 18:19:21 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -164,7 +164,7 @@ local plugins = {
     event = LazyBufEnter,
     opts = {
       -- Event to trigger linters
-      events = { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged", "BufEnter", "LazyFilePost" },
+      events = { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged", "BufEnter" },
       linters_by_ft = {
         fish = { "fish" },
         typescriptreact = { "eslint_d" },
