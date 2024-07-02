10a7d8afdf8fc36acf83f31eaad8d363874325bf
huzilong <huzilong@meituan.com>
2024-07-02 16:42:56 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -145,7 +145,7 @@ local plugins = {
         -- ["*"] = { "codespell" },
       },
       -- Set up format-on-save
-      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+      -- format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
       -- Customize formatters
       formatters = {
         shfmt = {
