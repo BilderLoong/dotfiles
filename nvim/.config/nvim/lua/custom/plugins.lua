8858621df5ebfd356137db54cdd400305977f703
huzilong <huzilong@meituan.com>
2024-07-03 16:23:00 +0800

    M nvim/.config/nvim/lua/custom/plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -196,9 +196,11 @@ local plugins = {
       },
     },
     config = function(_, opts)
+      local lint = require "lint"
+      
+
       local M = {}
 
-      local lint = require "lint"
       for name, linter in pairs(opts.linters) do
         if type(linter) == "table" and type(lint.linters[name]) == "table" then
           lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
