ffff5b2aaab935188befa66b526979ab51f5f13b
birudo <a514059172@gmail.com>
2026-05-19 21:23:51 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -69,10 +69,10 @@ zinit wait lucid for \
     has'exa' DarrinTisdale/zsh-aliases-exa \
     as"command" from"gh-r"  eval"zoxide init zsh" ajeetdsouza/zoxide \
     zdharma-continuum/fast-syntax-highlighting 
+    has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd"  zdharma-continuum/null  \
 
 # Meaning of `lucid`: https://zdharma-continuum.github.io/zinit/wiki/Example-Minimal-Setup/
 zinit wait lucid atload'zicompinit; zicdreplay' for \
-    has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd"  zdharma-continuum/null  \
     wait'!0' has'starship' id-as'starship_init' eval"starship init zsh"  zdharma-continuum/null  \
     has"thefuck" id-as"thefuck_init" eval"thefuck --alias"  zdharma-continuum/null \
     Aloxaf/fzf-tab  has'navi' id-as'navi-widget' eval"navi widget zsh"  zdharma-continuum/null \
