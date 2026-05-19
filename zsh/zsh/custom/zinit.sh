4433d36ee412ce8dc8dfb458a5e623aeeed92ebf
birudo <a514059172@gmail.com>
2026-05-19 21:21:51 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -70,8 +70,7 @@ zinit wait lucid atload'zicompinit; zicdreplay' for \
     has'exa' DarrinTisdale/zsh-aliases-exa \
     zdharma-continuum/fast-syntax-highlighting \
         has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd"  zdharma-continuum/null  \
-         wait'!0' has'starship' id-as'starship_init' eval"starship init zsh" \
-    zdharma-continuum/null  \
+         wait'!0' has'starship' id-as'starship_init' eval"starship init zsh"  zdharma-continuum/null  \
       has"thefuck" id-as"thefuck_init" eval"thefuck --alias" \
     zdharma-continuum/null \
     Aloxaf/fzf-tab \
