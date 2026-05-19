e8fba2bfedafdc0c34feaa2a82d50fec045af0c7
birudo <a514059172@gmail.com>
2026-05-19 20:10:50 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -56,7 +56,7 @@ zinit wait lucid atload'zicompinit; zicdreplay' for \
     zdharma-continuum/null \
         has'syncthing' id-as'syncthing-completions' eval"syncthing install-completions" \
     zdharma-continuum/null \
-        has'opencode' id-as'opencode-completions' as' nocompile \
+        has'opencode' id-as'opencode-completions' as'completion' nocompile \
             atclone'opencode completion > _opencode' atpull'%atclone' \
     zdharma-continuum/null 
 
