5aba8fcff939f5baa282a82383a6555132cfa07e
birudo <a514059172@gmail.com>
2026-05-19 21:23:45 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -67,7 +67,7 @@ zinit wait lucid for \
     has'git' OMZL::git.zsh \
     has'git' OMZP::git \
     has'exa' DarrinTisdale/zsh-aliases-exa \
-    as"command" from"gh-r"  eval"zoxide init zsh" ajeetdsouza/zoxide\
+    as"command" from"gh-r"  eval"zoxide init zsh" ajeetdsouza/zoxide \
     zdharma-continuum/fast-syntax-highlighting \
 
 # Meaning of `lucid`: https://zdharma-continuum.github.io/zinit/wiki/Example-Minimal-Setup/
