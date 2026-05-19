ae2b66b79db003305c66930b173ec216049d80c6
birudo <a514059172@gmail.com>
2026-05-19 16:40:06 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -61,8 +61,8 @@ zinit wait lucid atload'zicompinit; zicdreplay' for \
     zdharma-continuum/null \
         has'syncthing' id-as'syncthing-compleltions' eval"syncthing install-completions" \
     zdharma-continuum/null \
-has'opencode' id-as'opencode-completions' as'completion' \
-    atclone'opencode completion > _opencode' atpull'%atclone' \
+        has'opencode' id-as'opencode-completions' as'completion' \
+            atclone'opencode completion > _opencode' atpull'%atclone' \
 # Fish-like autosuggestions for zsh 
 zinit wait lucid atload'_zsh_autosuggest_start' for \
       zsh-users/zsh-autosuggestions 
