71cd337f387de4ad72b7719e6d8f6f49cf93a8c0
birudo <a514059172@gmail.com>
2026-05-19 20:23:09 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -71,11 +71,6 @@ zinit wait lucid atload'_zsh_autosuggest_start' for \
 zinit ice wait lucid atload'zicompinit; zicdreplay' as"command" from"gh-r"  eval"zoxide init zsh"
 zinit light ajeetdsouza/zoxide
 
-zinit ice id-as"opencode-completion" has"opencode" \
-    atclone"opencode completion zsh > _opencode; zinit creinstall -q opencode-completion" \
-    atpull"%atclone" 
-zinit light zdharma-continuum/null
-
 # Use `id-as` to avoid conflict.
 zinit wait"1" lucid for \
     has"pyenv" eval"pyenv init -" \
