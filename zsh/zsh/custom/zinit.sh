19b6daa26897885544f5cfd0267be1c5b5e4c8b6
birudo <a514059172@gmail.com>
2026-05-19 21:24:25 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -70,7 +70,7 @@ zinit wait lucid for \
     as"command" from"gh-r"  eval"zoxide init zsh" ajeetdsouza/zoxide \
     zdharma-continuum/fast-syntax-highlighting \
     has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd"  zdharma-continuum/null
-    wait'!0' has'starship' id-as'starship_init' eval"starship init zsh"  zdharma-continuum/null  
+    wait'!0' has'starship' id-as'starship_init' eval"starship init zsh"  zdharma-continuum/null
 
 # Meaning of `lucid`: https://zdharma-continuum.github.io/zinit/wiki/Example-Minimal-Setup/
 ## For the plugins with completions
