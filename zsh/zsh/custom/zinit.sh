a0b7f89e77d92b07c82ef62ad0ba91cc7668e892
birudo <a514059172@gmail.com>
2026-05-19 21:23:54 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- zsh/zsh/custom/zinit.sh
+++ zsh/zsh/custom/zinit.sh
@@ -69,7 +69,7 @@ zinit wait lucid for \
     has'exa' DarrinTisdale/zsh-aliases-exa \
     as"command" from"gh-r"  eval"zoxide init zsh" ajeetdsouza/zoxide \
     zdharma-continuum/fast-syntax-highlighting \
-    has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd"  zdharma-continuum/null  \
+    has"fnm" id-as'fnm_env' eval"fnm env --use-on-cd"  zdharma-continuum/null 
 
 # Meaning of `lucid`: https://zdharma-continuum.github.io/zinit/wiki/Example-Minimal-Setup/
 zinit wait lucid atload'zicompinit; zicdreplay' for \
