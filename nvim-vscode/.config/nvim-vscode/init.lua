093c77a05dac8ab82fc8472f3f4fb81c28d411df
birudo <a514059172@gmail.com>
2026-06-03 22:06:01 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -205,6 +205,7 @@ local function plugins()
 			-- before the plugin's FileType autocmd is registered. If this plugin loads
 			-- on BufReadPost, the initial buffers miss the textobject keymap attachment
 			-- and treesitter textobjects (daf, vif, etc.) silently do nothing.
+			-- event = "VeryLazy",
 			event = "VeryLazy",
 			dependencies = "nvim-treesitter/nvim-treesitter",
 		},
