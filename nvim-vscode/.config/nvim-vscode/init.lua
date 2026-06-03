e33bc46b5550db2eddb0129c42a515633256c279
birudo <a514059172@gmail.com>
2026-06-03 20:28:34 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -183,14 +183,14 @@ local function plugins()
 						"json",
 						"vue",
 						"go",
-						
+
 
 						-- low level
 						"c",
 						"zig",
-						
+
 						-- other
-						
+						"python"
 					},
 				}
 			end,
@@ -210,14 +210,14 @@ local function plugins()
 			event = "BufReadPost",
 			---@type Flash.Config
 			opts = {},
-       -- stylua: ignore
-      keys = {
-        { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-        { "m", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-      },
+			-- stylua: ignore
+			keys = {
+				{ "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
+				{ "m",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+				{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+				{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+				{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+			},
 		},
 		-- Doesn't in VSCodeNeovim due to dependencies of LSP: https://github.com/mfussenegger/nvim-treehopper/blob/5a28bff46c05d28bdb4bcaef67e046eb915a9390/lua/tsht.lua#L96
 		--[[ {
