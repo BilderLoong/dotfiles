8ec978004b6e0643edb0a9e324a0637eab6442fe
birudo <a514059172@gmail.com>
2026-06-19 20:27:23 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -219,7 +219,7 @@ local function plugins()
 				{ "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
 				{ "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
 				{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-				{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+				{ "R",     mode = { "o", "x" },      , desc = "Treesitter Search" },
 				{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
 			},
 		},
