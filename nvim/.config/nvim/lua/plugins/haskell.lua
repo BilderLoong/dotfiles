25536a41f90f1e5f9133d30b22f9ec746993dcfe
birudo <a514059172@gmail.com>
2026-06-20 16:26:05 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

---
+++ nvim/.config/nvim/lua/plugins/haskell.lua
@@ -0,0 +1,9 @@
+return {
+  "mrcjkb/haskell-tools.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  version = "^3",
+  ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+}
