3ff2a230b7a71a1678535801a7919c4415e0e52d
birudo <a514059172@gmail.com>
2026-06-20 16:34:47 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

---
+++ nvim/.config/nvim/lua/log.lua
@@ -0,0 +1,47 @@
+local log = require "structlog"
+
+local stack_level = 1
+
+log.configure {
+  debug_logger = {
+    pipelines = {
+      {
+        level = log.level.INFO,
+        processors = {
+          log.processors.StackWriter({ "line", "file" }, { max_parents = 0, stack_level = stack_level }),
+          log.processors.Timestamper "%H:%M:%S",
+        },
+        formatter = log.formatters.FormatColorizer(
+          "%s [%s] %s: %-30s",
+          { "timestamp", "level", "logger_name", "msg" },
+          { level = log.formatters.FormatColorizer.color_level() }
+        ),
+        sink = log.sinks.Console(),
+      },
+      {
+        level = log.level.WARN,
+        processors = {},
+        formatter = log.formatters.Format(
+          "%s",
+          { "msg" },
+          { blacklist = { "level", "logger_name" } }
+        ),
+        sink = log.sinks.NvimNotify(),
+      },
+      {
+        level = log.level.TRACE,
+        processors = {
+          log.processors.StackWriter({ "line", "file" }, { max_parents = 3, stack_level = stack_level }),
+          log.processors.Timestamper "%H:%M:%S",
+        },
+        formatter = log.formatters.Format(
+          "%s [%s] %s: %-30s",
+          { "timestamp", "level", "logger_name", "msg" }
+        ),
+        sink = log.sinks.File(vim.fn.stdpath "log" .. "/debug.log"),
+      },
+    },
+  },
+}
+
+return log.get_logger "debug_logger"
