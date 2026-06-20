2d4de75086cccb32bd45589262f4421b9e54388e
birudo <a514059172@gmail.com>
2026-06-20 16:31:37 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/bigfile.lua
+++ nvim/.config/nvim/lua/plugins/bigfile.lua
@@ -6,6 +6,7 @@ return {
       pattern = function(bufnr, filesize_mib)
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
         if not ok or not stats then return true end
+
         local buf_size = stats.size
         local is_big_oneliner = vim.api.nvim_buf_line_count(bufnr) == 1 and buf_size > 100 * 1024
         return is_big_oneliner or buf_size > 2000 * 1024
