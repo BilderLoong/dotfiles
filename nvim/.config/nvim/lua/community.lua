3ace5f78bc84cebcbaaf885c9ba39e87d298da32
birudo <a514059172@gmail.com>
2026-06-20 21:22:47 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -9,6 +9,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.recipes.vscode" },
-}
   -- import/override with your plugins folder
 }
