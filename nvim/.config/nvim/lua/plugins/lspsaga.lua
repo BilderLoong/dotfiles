6fbaee4a05c3ef14a30f828d776c358070700af4
birudo <a514059172@gmail.com>
2026-06-20 16:21:54 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

---
+++ nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,21 @@
+return {
+  "nvimdev/lspsaga.nvim",
+  event = "LspAttach",
+  config = function()
+    require("lspsaga").setup {
+      finder = {
+        max_height = 0.6,
+        left_width = 0.45,
+        right_width = 0.50,
+        keys = { vsplit = "v", split = "s" },
+        methods = { tyd = "textDocument/typeDefinition" },
+      },
+      lightbulb = { enable = false },
+      code_action = { extend_gitsigns = true },
+    }
+  end,
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
