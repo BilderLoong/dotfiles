54c3c3c6cb7dc9d121775821ed7c3fc0aa8c2be4
birudo <a514059172@gmail.com>
2026-06-20 16:27:26 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  "github/copilot.vim",
+  event = "User AstroFile",
+  cmd = "Copilot",
+  config = function()
+    vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_proxy = "localhost:7890"
+  end,
+}
---
+++ nvim/.config/nvim/lua/plugins/export-vscode.lua
@@ -0,0 +1,3 @@
+return {
+  "elijahmanor/export-to-vscode.nvim",
+}
