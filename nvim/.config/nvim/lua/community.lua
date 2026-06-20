3b4d0e4abbbbefd0a253c8910a515f1b2a78ee5d
birudo <a514059172@gmail.com>
2026-06-20 16:08:22 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,15 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+}
