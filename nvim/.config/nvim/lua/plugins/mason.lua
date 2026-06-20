6cbd9ceb8b362f47815d650d968f7ed9e34df5e9
birudo <a514059172@gmail.com>
2026-06-20 16:52:38 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,11 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    registries = {
-      "github:nvim-java/mason-registry",
-      "github:mason-org/mason-registry",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:nvim-java/mason-registry",
+        "github:mason-org/mason-registry",
+      },
     },
   },
   {
