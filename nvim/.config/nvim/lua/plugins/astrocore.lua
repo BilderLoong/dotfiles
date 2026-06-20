9393cad51d953365ac47e42c6311ccce50807cbb
birudo <a514059172@gmail.com>
2026-06-20 20:33:13 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -1,84 +1,79 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
--- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      large_buf = { size = 1024 * 256, lines = 10000 },
+      autopairs = true,
+      cmp = true,
+      diagnostics = { virtual_text = true, virtual_lines = false },
+      highlighturl = true,
+      notifications = true,
     },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
       virtual_text = true,
       underline = true,
     },
-    -- passed to `vim.filetype.add`
-    filetypes = {
-      -- see `:h vim.filetype.add` for usage
-      extension = {
-        foo = "fooscript",
-      },
-      filename = {
-        [".foorc"] = "fooscript",
-      },
-      pattern = {
-        [".*/etc/foo/.*"] = "fooscript",
-      },
-    },
-    -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
+      opt = {
+        relativenumber = true,
+        number = true,
+        spell = false,
+        signcolumn = "yes",
+        wrap = false,
       },
     },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
-      -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
+        -- Escape insert mode
+        ["jk"] = { "<ESC>", desc = "Escape insert mode", nowait = true },
 
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
+        -- Open config
+        ["<leader>cg"] = {
+          function() require("custom_cmds").open_config() end,
+          desc = "Open neovim config",
         },
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
+        -- Glance LSP
+        ["gd"] = { "<cmd>Glance definitions<CR>", desc = "Glance definitions" },
+        ["gr"] = { "<cmd>Glance references<CR>", desc = "Glance references" },
+        ["gy"] = { "<cmd>Glance type_definitions<CR>", desc = "Glance type definitions" },
+        ["gi"] = { "<cmd>Glance implementations<CR>", desc = "Glance implementations" },
 
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        -- Lspsaga
+        ["gO"] = { "<cmd>Lspsaga outline<CR>", desc = "Symbols outline" },
+        ["ga"] = { "<cmd>Lspsaga finder<CR>", desc = "LSP finder" },
+        ["K"] = { "<cmd>Lspsaga hover_doc<CR>", desc = "Hover doc" },
+        ["<leader>ca"] = { "<cmd>Lspsaga code_action<CR>", desc = "Code action" },
+        ["<leader>n"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next diagnostic" },
+
+        -- Session
+        ["<leader>ss"] = {
+          function() require("auto-session.session-lens").search_session() end,
+          desc = "Session lens",
+        },
+
+        -- Copilot accept (insert mode)
+        -- Note: this is in insert mode, see below
+
+        -- Yanky history navigation
+        ["<A-k>"] = { "<Plug>(YankyPreviousEntry)", desc = "Yanky previous" },
+        ["<A-j>"] = { "<Plug>(YankyNextEntry)", desc = "Yanky next" },
+
+        -- UFO folds
+        ["zM"] = { function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
+        ["zR"] = { function() require("ufo").openAllFolds() end, desc = "Open all folds" },
+
+        -- Quick find files
+        ["<C-P>"] = { function() require("snacks").picker.files() end, desc = "Find files" },
+      },
+      i = {
+        -- Copilot accept
+        ["<C-J>"] = { 'copilot#Accept("<CR>")', expr = true, replace_keycodes = false, desc = "Copilot accept" },
+      },
+      v = {
+        ["jk"] = { "<ESC>", desc = "Escape visual mode" },
       },
     },
   },
