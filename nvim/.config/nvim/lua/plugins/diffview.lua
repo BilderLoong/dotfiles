06e8f7e1e077eaf85e77d62e47b456b3e3773a66
birudo <a514059172@gmail.com>
2026-06-20 16:22:53 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

---
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
+  opts = {
+    enhanced_diff_hl = true,
+  },
+}
