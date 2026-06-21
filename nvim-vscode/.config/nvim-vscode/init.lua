e3f8155b71711fd9898b67ca85c3cd5eb61704a4
birudo <a514059172@gmail.com>
2026-06-21 15:50:27 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -59,7 +59,26 @@ local function plugins()
 		   ]])
 			end,
 		},
-
+{
+  "gbprod/substitute.nvim",
+  keys = {
+    { "<leader>mm", mode = { "n", "o", "x" }, "m", desc = "Default m key.", { noremap = true } },
+    { "mx", mode = { "n" }, function() require("substitute.exchange").operator() end, desc = "Substitute Exchange Operator", { noremap = true } },
+    { "mxx", mode = { "n" }, function() require("substitute.exchange").line() end, desc = "Substitute Exchange Line", { noremap = true } },
+    { "X", mode = { "x" }, function() require("substitute.exchange").visual() end, desc = "Substitute Exchange Visual", { noremap = true } },
+    { "mxc", mode = { "n" }, function() require("substitute.exchange").cancel() end, desc = "Substitute Exchange Cancel", { noremap = true } },
+    { "m", mode = { "n" }, function() require("substitute").operator() end, desc = "Substitute Operator", { noremap = true } },
+    { "mm", mode = { "n" }, function() require("substitute").line() end, desc = "Substitute Line", { noremap = true } },
+    { "M", mode = { "n" }, function() require("substitute").eol() end, desc = "Substitute EOL", { noremap = true } },
+    { "m", mode = { "x" }, function() require("substitute").visual() end, desc = "Substitute Visual", { noremap = true } },
+  },
+  opts = {},
+  config = function(_, opts)
+    require("substitute").setup {
+      on_substitute = require("yanky.integration").substitute(),
+    }
+  end,
+}
 		{
 			"kylechui/nvim-surround",
 			version = "*", -- Use for stability; omit to use `main` branch for the latest features
