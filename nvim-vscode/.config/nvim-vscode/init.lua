0bbee8af499d07f55e37b13afdc20e7cb0bf3a72
birudo <a514059172@gmail.com>
2026-06-21 20:39:09 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -185,7 +185,7 @@ local function plugins()
 							init_selection = "\\",
 							-- TODO conflict with `gr`
 							node_incremental = "\\",
-							scope_incremental = "<bs>",
+							scope_incremental = "",
 							node_decremental = "gnm",
 						},
 					},
