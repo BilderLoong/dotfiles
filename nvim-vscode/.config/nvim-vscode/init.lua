efe8f3e9f2a4f93c6615c2081a7b0105c8505c14
birudo <a514059172@gmail.com>
2026-06-21 20:07:06 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim-vscode/.config/nvim-vscode/init.lua
+++ nvim-vscode/.config/nvim-vscode/init.lua
@@ -64,6 +64,7 @@ local function plugins()
 			keys = {
 				{ "<leader>mm", mode = { "n", "o", "x" }, "m",                                                      desc = "Default m key.",               { noremap = true } },
 				{ "m",          mode = { "n" },           function() require("substitute").operator() end,          desc = "Substitute Operator",          { noremap = true } },
+				{ "m",          mode = { "x" },           function() require("substitute").visual() end,            desc = "Substitute Visual",            { noremap = true } },
 				{ "mx",         mode = { "n" },           function() require("substitute.exchange").operator() end, desc = "Substitute Exchange Operator", { noremap = true } },
 				{ "mxx",        mode = { "n" },           function() require("substitute.exchange").line() end,     desc = "Substitute Exchange Line",     { noremap = true } },
 				{ "X",          mode = { "x" },           function() require("substitute.exchange").visual() end,   desc = "Substitute Exchange Visual",   { noremap = true } },
