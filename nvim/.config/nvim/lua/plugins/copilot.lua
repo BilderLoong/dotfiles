8b0310b31b76e50c7fec8157b3756cb81d9a2306
birudo <a514059172@gmail.com>
2026-06-21 12:38:42 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "github/copilot.vim",
   event = "User AstroFile",
-  enabled=fale
+  enabled=false,
   cmd = "Copilot",
   config = function()
     vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', {
