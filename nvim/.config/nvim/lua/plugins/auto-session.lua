374d1d1c5fac25d568916ca8e6a957362bf46dee
birudo <a514059172@gmail.com>
2026-08-29 21:12:19 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,40 +1,37 @@
 return {
-	"rmagatti/auto-session",
-	event = "VeryLazy",
-	cmd = {
-		"AutoSession",
-		"Autosession",
-		"SessionDelete",
-		"SessionRestore",
-		"SessionRestoreFromFile",
-		"SessionSave",
-	},
-	keys = {
-		{
-			"<Leader>fS",
-			function()
-				require("auto-session").search()
-			end,
-			desc = "Search sessions",
-		},
-	},
-	opts = {
-		auto_restore_last_session = true,
-		auto_restore_enabled = true,
-		log_level = vim.log.levels.ERROR,
-		session_lens = {
-			load_on_setup = true,
-			picker = "snacks",
-			theme_conf = { border = true },
-			previewer = true,
-		},
-		pre_save_cmds = {
-			"lua if vim.fn.exists(':NvimTreeClose') > 0 then vim.cmd('tabdo NvimTreeClose') end",
-			"lua if vim.fn.exists(':DiffviewClose') > 0 then vim.cmd('tabdo DiffviewClose') end",
-		},
-	},
-	config = function(_, opts)
-		require("auto-session").setup(opts)
-		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-	end,
+  "rmagatti/auto-session",
+  event = "VeryLazy",
+  cmd = {
+    "AutoSession",
+    "Autosession",
+    "SessionDelete",
+    "SessionRestore",
+    "SessionRestoreFromFile",
+    "SessionSave",
+  },
+  keys = {
+    {
+      "<Leader>fS",
+      function() require("auto-session").search() end,
+      desc = "Search sessions",
+    },
+  },
+  opts = {  auto_restore_last_session = true,
+    auto_restore_enabled = false,
+    log_level = vim.log.levels.ERROR,
+    session_lens = {
+      load_on_setup = true,
+      picker = "snacks",
+      theme_conf = { border = true },
+      previewer = true,
+    },
+    pre_save_cmds = {
+      "lua if vim.fn.exists(':NvimTreeClose') > 0 then vim.cmd('tabdo NvimTreeClose') end",
+      "lua if vim.fn.exists(':DiffviewClose') > 0 then vim.cmd('tabdo DiffviewClose') end",
+    },
+  },
+  config = function(_, opts)
+    require("auto-session").setup(opts)
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+  end,
 }
