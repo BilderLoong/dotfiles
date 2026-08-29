f6c54fbcb6acef9daf3fd637d1348749cb195418
birudo <a514059172@gmail.com>
2026-08-29 20:50:29 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,1 @@
-return {
-	"MeanderingProgrammer/treesitter-modules.nvim",
-	lazy = false,
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	opts = {
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "\\",
-				node_incremental = "\\",
-				scope_incremental = false,
-				node_decremental = "<BS>",
-			},
-		},
-	},
-}
+return {}
