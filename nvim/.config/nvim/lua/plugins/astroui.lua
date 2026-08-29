f69bb62acec8b2b579e26d76282642fb331f8176
birudo <a514059172@gmail.com>
2026-08-29 20:44:00 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -5,43 +5,45 @@
 
 ---@type LazySpec
 return {
-  {
-    "gbprod/nord.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function(_, opts) require("nord").setup(opts) end,
-  },
-  {
-    "AstroNvim/astroui",
-    ---@type AstroUIOpts
-    opts = {
-      -- change colorscheme
-      colorscheme = "nord",
-      -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-      highlights = {
-        init = { -- this table overrides highlights in all themes
-          -- Normal = { bg = "#000000" },
-        },
-        nord = { -- a table of overrides/changes when applying the Nord theme
-          Comment = { fg = "#646E86", italic = true },
-          ["@comment"] = { fg = "#646E86", italic = true },
-        },
-      },
-      -- Icons can be configured throughout the interface
-      icons = {
-        -- configure the loading of the lsp in the status line
-        LSPLoading1 = "⠋",
-        LSPLoading2 = "⠙",
-        LSPLoading3 = "⠹",
-        LSPLoading4 = "⠸",
-        LSPLoading5 = "⠼",
-        LSPLoading6 = "⠴",
-        LSPLoading7 = "⠦",
-        LSPLoading8 = "⠧",
-        LSPLoading9 = "⠇",
-        LSPLoading10 = "⠏",
-      },
-    },
-  },
+	{
+		"gbprod/nord.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function(_, opts)
+			require("nord").setup(opts)
+		end,
+	},
+	{
+		"AstroNvim/astroui",
+		---@type AstroUIOpts
+		opts = {
+			-- change colorscheme
+			colorscheme = "nord",
+			-- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+			highlights = {
+				init = { -- this table overrides highlights in all themes
+					-- Normal = { bg = "#000000" },
+				},
+				nord = { -- a table of overrides/changes when applying the Nord theme
+					Comment = { fg = "#646E86", italic = true },
+					["@comment"] = { fg = "#646E86", italic = true },
+				},
+			},
+			-- Icons can be configured throughout the interface
+			icons = {
+				-- configure the loading of the lsp in the status line
+				LSPLoading1 = "⠋",
+				LSPLoading2 = "⠙",
+				LSPLoading3 = "⠹",
+				LSPLoading4 = "⠸",
+				LSPLoading5 = "⠼",
+				LSPLoading6 = "⠴",
+				LSPLoading7 = "⠦",
+				LSPLoading8 = "⠧",
+				LSPLoading9 = "⠇",
+				LSPLoading10 = "⠏",
+			},
+		},
+	},
 }
