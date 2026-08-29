b586035e4724b82d8194ab351b60832fe1662a89
birudo <a514059172@gmail.com>
2026-08-29 20:52:08 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,7 +5,7 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
   -- TODO: try this: https://docs.astronvim.com/recipes/mappings/#enable-picker-lsp-mappings
   -- { import = "astrocommunity.recipes.picker-lsp-mappings" },
   -- TODO: Config to use with vscode.
