425a5ded2e34d7ae16b5e2c2b7997f3e1bf7bce1
birudo <a514059172@gmail.com>
2026-08-30 21:28:16 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,7 +4,7 @@ return {
 	cmd = { "ConformInfo" },
 	keys = {
 		{
-			"<leader>lm",
+			"<leader>fm",
 			function()
 				require("conform").format({ async = true, lsp_format = "fallback" })
 			end,
