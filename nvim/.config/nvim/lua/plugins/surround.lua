25a76f7ed11aa64883d86e9c23b16c3fc5d47134
birudo <a514059172@gmail.com>
2026-08-30 17:33:20 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,6 +1,9 @@
 return {
   "kylechui/nvim-surround",
   version = "*",
+  init = function()
+    vim.g.nvim_surround_no_visual_mappings = true
+  end,
   keys = {
     { "<C-g>s", mode = "i" },
     { "<C-g>S", mode = "i" },
@@ -12,13 +15,13 @@ return {
     { "gs", mode = "x" },
     { "gS", mode = "x" },
   },
-  opts = {
-    keymaps = {
-      visual = "gs",
-      visual_line = "gS",
-    },
-  },
-  config = function(_, opts)
-    require("nvim-surround").setup(opts)
+  config = function()
+    require("nvim-surround").setup {}
+    vim.keymap.set("x", "gs", "<Plug>(nvim-surround-visual)", {
+      desc = "Add a surrounding pair around a visual selection",
+    })
+    vim.keymap.set("x", "gS", "<Plug>(nvim-surround-visual-line)", {
+      desc = "Add a surrounding pair around a visual selection, on new lines",
+    })
   end,
 }
