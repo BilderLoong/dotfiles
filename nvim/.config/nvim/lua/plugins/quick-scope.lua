ae7392abf516efa1102f27b985af126a9ebe6439
birudo <a514059172@gmail.com>
2026-08-30 20:35:23 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/quick-scope.lua
+++ nvim/.config/nvim/lua/plugins/quick-scope.lua
@@ -5,6 +5,6 @@ return {
     -- vim.cmd [[
 			  " highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
 			  " highlight QuickScopeSecondary guifg='#6fffff' gui=underline ctermfg=81 cterm=underline
-   ]]
+   -- ]]
   end,
 }
