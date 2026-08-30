becfaaded5a35af574d165af1ea93143a97dd422
birudo <a514059172@gmail.com>
2026-08-30 20:35:22 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/quick-scope.lua
+++ nvim/.config/nvim/lua/plugins/quick-scope.lua
@@ -2,7 +2,7 @@ return {
   "unblevable/quick-scope",
   event = "BufReadPost",
   config = function()
-    vim.cmd [[
+    -- vim.cmd [[
 			  " highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
 			  " highlight QuickScopeSecondary guifg='#6fffff' gui=underline ctermfg=81 cterm=underline
    ]]
