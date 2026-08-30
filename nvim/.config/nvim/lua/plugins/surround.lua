d1c1ac34156efc74fc6be6874ae16e28ae98639e
birudo <a514059172@gmail.com>
2026-08-30 17:32:02 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,8 +1,24 @@
 return {
   "kylechui/nvim-surround",
   version = "*",
-  keys = { "c", "y", "d" },
-  config = function()
-    require("nvim-surround").setup {}
+  keys = {
+    { "<C-g>s", mode = "i" },
+    { "<C-g>S", mode = "i" },
+    { "ys", mode = "n" },
+    { "yS", mode = "n" },
+    { "ds", mode = "n" },
+    { "cs", mode = "n" },
+    { "cS", mode = "n" },
+    { "gs", mode = "x" },
+    { "gS", mode = "x" },
+  },
+  opts = {
+    keymaps = {
+      visual = "gs",
+      visual_line = "gS",
+    },
+  },
+  config = function(_, opts)
+    require("nvim-surround").setup(opts)
   end,
 }
