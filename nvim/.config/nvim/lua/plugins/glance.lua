40f95b06f96d7cd1b7405f08d4f111eb0a5d8aff
birudo <a514059172@gmail.com>
2026-08-31 21:54:25 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/glance.lua
+++ nvim/.config/nvim/lua/plugins/glance.lua
@@ -9,17 +9,6 @@ return
 
   config = function()
     require("glance").setup {
-      hooks = {
-        before_open = function(results, open, jump)
-          if #results == 1 then
-            jump(results[1])
-          else
-            open(results)
-          end
-        end,
-      },
-      border = { enable = true },
-      use_trouble_qf = true,
     }
   end,
 }
