5c224c47c1498ad5acf08a41099bbdcf0a195d7c
birudo <a514059172@gmail.com>
2026-08-31 17:41:28 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -40,7 +40,7 @@ return {
     maps.n["<Leader>fA"] = { function() require("snacks").picker.ast_grep() end, desc = "Find AST patterns" }
     maps.n["<Leader>fM"] = { function() require("snacks").picker.man() end, desc = "Find man" }
     maps.n["<Leader>ff"] = { function() require("snacks").picker.resume() end, desc = "Resume previous search" }
-    maps.n["<Leader>ff"] = { function() require("snacks").picker.resume() end, desc = "Resume previous search" }
+    maps.n["<Leader>ff"] = { function() require("snacks").picker.pickers() end, desc = "Resume previous search" }
 
     maps.n.gL = { function() require("snacks").picker.diagnostics() end, desc = "Search diagnostics" }
     -- maps.n.gL = {
