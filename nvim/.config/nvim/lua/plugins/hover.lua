d39f8c553343e99d4b019b317ea0978d4104f36e
birudo <a514059172@gmail.com>
2026-08-31 16:45:29 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/hover.lua
+++ nvim/.config/nvim/lua/plugins/hover.lua
@@ -22,14 +22,14 @@ return
       mouse_delay = 1000,
     }
   end,
-  keys = {
-    {
+    keys = 
       {
-        "<MouseMove>",
-        mode = { "n" },
-        function() require("hover").mouse() end,
-        desc = "hover.nvim (mouse)",
+        {
+          "<MouseMove>",
+          mode = { "n" },
+          function() require("hover").mouse() end,
+          desc = "hover.nvim (mouse)",
+        },
       },
-    },
-  },
+    ,
 }
