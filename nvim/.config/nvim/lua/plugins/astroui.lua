4fbff18bb723031b063b9b4ca0810ccb373f622f
birudo <a514059172@gmail.com>
2026-09-01 17:28:18 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -27,6 +27,14 @@ return {
         nord = { -- a table of overrides/changes when applying the Nord theme
           Comment = { fg = "#75809C", italic = true },
           ["@comment"] = { fg = "#75809C", italic = true },
+          -- NvChad-era diff colors, ported from the pre-AstroNvim config
+          -- (nvim/.config/nvim/lua/custom/chadrc.lua @ f7ce9e03): hl_override used
+          -- bg = { "green", -25 } etc., which base46 resolves through
+          -- change_hex_lightness against the Nord palette.
+          DiffAdd = { bg = "#628149", fg = "NONE" },
+          DiffDelete = { bg = "#BC5963", fg = "NONE" },
+          DiffChange = { bg = "#906819", fg = "NONE" },
+          DiffText = { bg = "#BC8820", fg = "NONE" },
         },
       },
       -- Icons can be configured throughout the interface
