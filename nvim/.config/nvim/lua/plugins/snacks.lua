68ff1d6942a4eedb12048a4ed1dd96b05fc9f178
birudo <a514059172@gmail.com>
2026-09-01 17:31:24 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -123,7 +123,8 @@ return {
 
     git_log_source.config = function(picker_opts)
       if upstream_git_log_config then 
-        picker_opts = upstream_git_log_config(picker_opts) or picker_opts end
+        picker_opts = upstream_git_log_config(picker_opts)
+        or picker_opts end
       if not picker_opts.current_file then return picker_opts end
 
       picker_opts.previewers.diff.style = "terminal"
