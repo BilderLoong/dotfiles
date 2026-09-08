655cc4f33c2b94675de7a041e0edf4944b049d99
birudo <a514059172@gmail.com>
2026-09-08 15:25:49 +0800

    git-annex in birudo@birudonoMacBook-Air.local:~/Projects/dotfiles

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -24,6 +24,7 @@ return {
           -- Normal = { bg = "#000000" },
         },
         nord = { -- a table of overrides/changes when applying the Nord theme
+          LspInlayHint = { fg = "#81A1C1", bg = "#353D4B", italic = false },
           -- Comment = { fg = "#75809C", italic = true },
           -- ["@comment"] = { fg = "#75809C", italic = true },
           -- NvChad-era diff colors, ported from the pre-AstroNvim config
